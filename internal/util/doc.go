3c552dcb672acd26f81c323dc03946fed9a5541f
David Hyland-Wood <david.wood@consensys.net>
2020-11-27 09:22:37 +1000

    Infrastructure framework: Make, Docker, CircleCI and test files.

---
+++ internal/util/doc.go
@@ -0,0 +1,6 @@
+/*
+Package internal contains a range of utility code. This code should probably be put into a library to be shared between
+applications at some point.
+ */
+package util
+
---
+++ pkg/client/network.go
@@ -0,0 +1,56 @@
+package client
+
+/*
+ * Copyright 2020 ConsenSys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import (
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/tatsushid/go-fastping"
+)
+
+// Ping pings a remote server via ICMP
+func Ping(pingserver string) bool {
+
+	if len(pingserver) == 0 {
+		log.Println("Error: Cannot ping empty servername")
+		os.Exit(1)
+	} else {
+		log.Println("Attempting to ping " + pingserver)
+	}
+
+	p := fastping.NewPinger()
+	ra, err := net.ResolveIPAddr("ip4:icmp", pingserver)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	p.AddIPAddr(ra)
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		log.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+	}
+	p.OnIdle = func() {
+		log.Println("finish")
+	}
+	err = p.Run()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
