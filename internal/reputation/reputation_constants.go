415c3fa3cc6b0f774760a50575b72579c88569e4
Peter  Robinson <drinkcoffee@eml.cc>
2020-11-30 09:56:15 +1000

    Set limits to client reputaiton
    Signed-off-by: Peter  Robinson <drinkcoffee@eml.cc>

--- internal/reputation/reputation_constants.go
+++ internal/reputation/reputation_constants.go
@@ -2,6 +2,8 @@ package reputation
 // Copyright (C) 2020 ConsenSys Software Inc
 
 
+const clientMaxReputation = 10000
+const clientMinReputaiton = -10000
 const clientInitialReputation = 10
 const clientEstablishmentChallenge = -1
 const clientOnChainDeposit = 1000
