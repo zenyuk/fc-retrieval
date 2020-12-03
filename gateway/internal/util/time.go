84a07ba52675876a3809916d34af6629df7c9374
Peter  Robinson <drinkcoffee@eml.cc>
2020-12-03 12:16:37 +1000

    Added mock time, test for expiring offers, plus fixed edge case when expiring offers and there is only one offer
    Signed-off-by: Peter  Robinson <drinkcoffee@eml.cc>

--- gateway/internal/util/time.go
+++ gateway/internal/util/time.go
@@ -10,3 +10,56 @@ func GetTimeNowString() string {
 	t := time.Now()
 	return t.Format("2006-01-02 15:04:05")
 }
+
+
+// Clock allows the time.Now to be mocked out for testing
+type Clock interface {
+	Now() time.Time
+//	After(d time.Duration) <-chan time.Time
+}
+
+
+var clock Clock
+
+
+// GetTimeImpl returns the implementation of clock to use.
+func GetTimeImpl() (Clock){
+	if (clock == nil) {
+		SetRealClock()
+	}
+	return clock
+}
+
+// SetRealClock ensures the real clock is in use
+func SetRealClock() {
+	clock = newRealClock()
+}
+
+
+func newRealClock() (impl Clock) {
+	r := realClock{}
+	var _ Clock = &r  // Enforce interface compliance
+	return &r
+}
+type realClock struct{}
+func (realClock) Now() time.Time { return time.Now() }
+//	func (realClock) After(d time.Duration) <-chan time.Time { return time.After(d) }
+
+
+var mockedUnixTime int64
+
+// SetMockedClock sets a fake timer
+func SetMockedClock(fakeTime int64) {
+	mockedUnixTime = fakeTime
+	clock = newMockedClock()
+}
+
+func newMockedClock() (impl Clock) {
+	r := mockedClock{}
+	var _ Clock = &r  // Enforce interface compliance
+	return &r
+}
+type mockedClock struct {}
+func (mockedClock) Now() time.Time { 
+	return time.Unix(mockedUnixTime, 0)
+}
