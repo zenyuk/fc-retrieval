c2649ac30465f61c1ff03bb9b6da0166ef41b6ab
Raphaël Pralat <raphael.pralat@consensys.net>
2021-01-18 16:21:27 +0100

    feat: new api for registers

---
+++ register/Makefile
@@ -0,0 +1,13 @@
+VERSION?=0.0.1
+
+start:
+	docker-compose up
+
+stop:
+	docker-compose stop
+
+build:
+	docker-compose build
+
+build-dev:
+	go build -v cmd/filecoin-retrieval-register-server/main.go
---
+++ register/internal/handlers/registers_handler.go
@@ -0,0 +1,65 @@
+package handlers
+
+import (
+	"context"
+
+	"encoding/json"
+
+	middleware "github.com/go-openapi/runtime/middleware"
+	redis "github.com/go-redis/redis/v8"
+	"github.com/rs/zerolog/log"
+
+	"github.com/ConsenSys/fc-retrieval-register/models"
+	op "github.com/ConsenSys/fc-retrieval-register/restapi/operations/registers"
+)
+
+var redisHash = "registers"
+
+// Add a register
+func AddRegister(params op.AddRegisterParams) middleware.Responder {
+	register := params.Register
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     apiconfig.GetString("REDIS_URL") + ":" + apiconfig.GetString("REDIS_PORT"),
+		Password: apiconfig.GetString("REDIS_PASSWORD"),
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.HSet(ctx, redisHash, register, 0).Err()
+	if err != nil {
+		log.Error().Msg("Unable to set Redis value")
+		panic(err)
+	}
+
+	log.Info().Msg("Register created")
+
+	// Response
+	return op.NewAddRegisterOK().WithPayload(register)
+}
+
+// Get register list
+func GetRegisters(params op.GetRegistersParams) middleware.Responder {
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     apiconfig.GetString("REDIS_URL") + ":" + apiconfig.GetString("REDIS_PORT"),
+		Password: apiconfig.GetString("REDIS_PASSWORD"),
+		DB:       0,  // use default DB
+	})
+	
+	registers, err := rdb.HGetAll(ctx, redisHash).Result()
+	if err != nil {
+		log.Error().Msg("Unable to get Redis value")
+		panic(err)
+	}
+
+	payload :=  []*models.Register{}
+	for registerJson, _ := range registers {
+		registerData := models.Register{}
+		json.Unmarshal([]byte(registerJson), &registerData)
+		payload = append(payload, &registerData)
+	}
+
+	return op.NewGetRegistersOK().WithPayload(payload)
+}
