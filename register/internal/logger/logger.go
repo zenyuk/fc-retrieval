1e96d178a4d614466df6517eeb8dee64e6fc9278
ssaidali <samir.saidali@consensys.net>
2021-01-19 19:56:39 +0000

    Fix formatting

--- register/internal/logger/logger.go
+++ register/internal/logger/logger.go
@@ -15,14 +15,14 @@ func setTimeFormat() {
 }
 
 func setLogLevel(conf *viper.Viper) {
-	logLevel := conf.GetString("LOGGING_LEVEL")
-	switch logLevel {
-		case "debug": zerolog.SetGlobalLevel(zerolog.DebugLevel)
-		case "trace": zerolog.SetGlobalLevel(zerolog.TraceLevel)
-		case "warn":	zerolog.SetGlobalLevel(zerolog.WarnLevel)
-		case "error": zerolog.SetGlobalLevel(zerolog.ErrorLevel)
-		case "fatal": zerolog.SetGlobalLevel(zerolog.FatalLevel)
-		case "panic": zerolog.SetGlobalLevel(zerolog.PanicLevel)
-		default: 			zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	}
+  logLevel := conf.GetString("LOGGING_LEVEL")
+  switch logLevel {
+    case "debug": zerolog.SetGlobalLevel(zerolog.DebugLevel)
+    case "trace": zerolog.SetGlobalLevel(zerolog.TraceLevel)
+    case "warn":	zerolog.SetGlobalLevel(zerolog.WarnLevel)
+    case "error": zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+    case "fatal": zerolog.SetGlobalLevel(zerolog.FatalLevel)
+    case "panic": zerolog.SetGlobalLevel(zerolog.PanicLevel)
+    default: 			zerolog.SetGlobalLevel(zerolog.InfoLevel)
+  }
 }
