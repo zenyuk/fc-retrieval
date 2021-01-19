5a378fca051a122aba3da05f3530eed585fbbff7
ssaidali <samir.saidali@consensys.net>
2021-01-19 19:55:19 +0000

    Clean code

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -19,7 +19,7 @@ func setLogLevel(conf *viper.Viper) {
 	switch logLevel {
 		case "debug": zerolog.SetGlobalLevel(zerolog.DebugLevel)
 		case "trace": zerolog.SetGlobalLevel(zerolog.TraceLevel)
-		case "warn": 	zerolog.SetGlobalLevel(zerolog.WarnLevel)
+		case "warn":	zerolog.SetGlobalLevel(zerolog.WarnLevel)
 		case "error": zerolog.SetGlobalLevel(zerolog.ErrorLevel)
 		case "fatal": zerolog.SetGlobalLevel(zerolog.FatalLevel)
 		case "panic": zerolog.SetGlobalLevel(zerolog.PanicLevel)
