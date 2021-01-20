231fc334af6a0726446d201612eb58158f426d0e
ssaidali <samir.saidali@consensys.net>
2021-01-20 23:10:22 +0000

    Logger integration

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -5,13 +5,12 @@ import (
 	log "github.com/ConsenSys/fc-retrieval-gateway/pkg/logging"
 
 	"github.com/ConsenSys/fc-retrieval-provider/config"
-	"github.com/ConsenSys/fc-retrieval-provider/internal/logging"
 	"github.com/ConsenSys/fc-retrieval-provider/internal/services/provider"
 )
 
 func main() {
   conf := config.NewConfig()
-  logging.InitLogger(conf)
+  log.Init(conf)
 
 	log.Info("Running app ...")
 	p := provider.NewProvider(conf)
