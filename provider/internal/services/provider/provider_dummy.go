28f38956c540f48f5c46f82030a16e69003b9374
Raphaël Pralat <raphael.pralat@consensys.net>
2021-01-20 16:57:38 +0100

    fix: docker network

--- provider/internal/services/provider/provider_dummy.go
+++ provider/internal/services/provider/provider_dummy.go
@@ -3,37 +3,46 @@ package provider
 import (
 	"math/rand"
 	"time"
+
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/cid"
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/nodeid"
 )
 
 // CIDMessage data model
 type CIDMessage struct {
-	ProtocolVersion   string
-	ProtocolSupported string
-	MessageType       string
-	Nonce             int
-	ProviderID        string
-	PricePerByte      int
-	ExpiryDate        int64
-	QosMetric         int
-	Signature         string
-	PieceCID          []string
+	MessageType       int32           `json:"message_type"`
+	ProtocolVersion   int32           `json:"protocol_version"`
+	ProtocolSupported []int32         `json:"protocol_supported"`
+	Nonce             int64           `json:"nonce"`
+	ProviderID        nodeid.NodeID   `json:"provider_id"`
+	Price             uint64          `json:"price_per_byte"`
+	Expiry            int64           `json:"expiry_date"`
+	QoS               uint64          `json:"qos"`
+	Signature         string          `json:"signature"`
+	PieceCIDs         []cid.ContentID `json:"piece_cids"`
 }
 
 func generateDummyMessage() CIDMessage {
 	expiryDate := time.Now().Local().Add(time.Hour * time.Duration(24)).Unix()
 
-	pieceCIDs := []string{"a", "b", "c", "d", "e"}
+	protocolSupported := []int32{1, 2}
+	providerID, _ := nodeid.NewRandomNodeID()
+
+	contentID, _ := cid.NewRandomContentID()
+	// pieceCIDs := []string{"a", "b", "c", "d", "e"}
+	pieceCIDs := []cid.ContentID{*contentID}
 	dummyMessage := CIDMessage{
-		ProtocolVersion:   "ProtocolVersion",
-		ProtocolSupported: "ProtocolSupported",
-		MessageType:       "MessageType",
-		Nonce:             rand.Intn(100000),
-		ProviderID:        "ProviderID",
-		PricePerByte:      42,
-		ExpiryDate:        expiryDate,
-		QosMetric:         42,
+
+		MessageType:       123,
+		ProtocolVersion:   1,
+		ProtocolSupported: protocolSupported,
+		Nonce:             rand.Int63n(100000),
+		ProviderID:        *providerID,
+		Price:             42,
+		Expiry:            expiryDate,
+		QoS:               42,
 		Signature:         "Signature",
-		PieceCID:          pieceCIDs,
+		PieceCIDs:         pieceCIDs,
 	}
 	return dummyMessage
 }
