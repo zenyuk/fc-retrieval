89eb6459e1f91e999c8f5123d737ea669afce033
wcgcyx <wcgcyx@gmail.com>
2021-01-22 17:34:56 +1000

    Replace msg in provider api with fcrmsg (#47)

--- internal/api/providerapi/publish_groupcid.go
+++ internal/api/providerapi/publish_groupcid.go
@@ -5,18 +5,25 @@ import (
 
 	"github.com/ConsenSys/fc-retrieval-gateway/internal/gateway"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/cidoffer"
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrmessages"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/logging"
-	"github.com/ConsenSys/fc-retrieval-gateway/pkg/messages"
 )
 
-func handleProviderPublishGroupCIDRequest(conn net.Conn, request *messages.ProviderPublishGroupCIDRequest) error {
+func handleProviderPublishGroupCIDRequest(conn net.Conn, request *fcrmessages.FCRMessage) error {
+	// Get the core structure
 	g := gateway.GetSingleInstance()
+
+	_, providerID, price, expiry, qos, cids, err := fcrmessages.DecodeProviderPublishGroupCIDRequest(request)
+	if err != nil {
+		return err
+	}
+
 	if g.Offers.Add(&cidoffer.CidGroupOffer{
-		NodeID:               &request.ProviderID,
-		Cids:                 request.PieceCIDs,
-		Price:                request.Price,
-		Expiry:               request.Expiry,
-		QoS:                  request.QoS,
+		NodeID:               providerID,
+		Cids:                 cids,
+		Price:                price,
+		Expiry:               expiry,
+		QoS:                  qos,
 		Signature:            request.Signature,
 		MerkleProof:          "TODO", //TODO: Who should be genearting the merkle proof?
 		FundedPaymentChannel: true,   //TODO: Need to check if the gateway has a payment channel established with the provider.
