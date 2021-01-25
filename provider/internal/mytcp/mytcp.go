56783a341698f66f4e0616a7ea6ab6380be75545
ssaidali <samir.saidali@consensys.net>
2021-01-25 11:03:35 +0000

    feat: send many messages

--- provider/internal/mytcp/mytcp.go
+++ provider/internal/mytcp/mytcp.go
@@ -17,34 +17,36 @@ import (
 
 // SendMessage to gateway
 func SendMessage() {
-	providerID, _ := nodeid.NewRandomNodeID()
-	contentID, _ := cid.NewRandomContentID()
-	pieceCIDs := []cid.ContentID{*contentID}
 	conn, err := net.Dial("tcp", "localhost:8090")
 	if err != nil {
 		log.Error("Fail to dial")
 		os.Exit(1)
 	}
-	request := messages.ProviderPublishGroupCIDRequest{
-		MessageType:       messages.ProviderPublishGroupCIDRequestType,
-		ProtocolVersion:   1,
-		ProtocolSupported: []int32{1},
-		Nonce:             1,                                       // TODO, Add nonce
-		ProviderID:        *providerID,
-		Price:						 100,
-		Expiry:						 time.Now().Add(10 * time.Second).Unix(),
-		QoS:							 50,
-		PieceCIDs:         pieceCIDs,
-	}
-	err = tcpcomms.SendMessageWithType(
-		conn,
-		messages.ProviderPublishGroupCIDRequestType,
-		&request,
-		30000)
 
-	fmt.Println("MY MESSAGE", request)
-	fmt.Println("-------------------------------------------")
-	fmt.Println(err)
+	for i := 0; i < 5; i++ {
+		providerID, _ := nodeid.NewRandomNodeID()
+		contentID, _ := cid.NewRandomContentID()
+		pieceCIDs := []cid.ContentID{*contentID}
+		request := messages.ProviderPublishGroupCIDRequest{
+			MessageType:       messages.ProviderPublishGroupCIDRequestType,
+			ProtocolVersion:   1,
+			ProtocolSupported: []int32{1},
+			Nonce:             int64(i),                                       // TODO, Add nonce
+			ProviderID:        *providerID,
+			Price:						 100,
+			Expiry:						 time.Now().Add(10 * time.Second).Unix(),
+			QoS:							 50,
+			PieceCIDs:         pieceCIDs,
+		}
+		err = tcpcomms.SendMessageWithType(
+			conn,
+			messages.ProviderPublishGroupCIDRequestType,
+			&request,
+			30000)
+		fmt.Println("MY MESSAGE", request)
+		fmt.Println("-------------------------------------------")
+		fmt.Println(err)
+	}
 
 	// if err != nil && tcpcomms.IsTimeoutError(err) {
 	// 	// Timeout can be ignored. Since this message can expire.
