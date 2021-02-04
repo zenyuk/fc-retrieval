c9bb6f0608e92e04e6b6b499dcc07df40cd4406d
Raphaël Pralat <raphael.pralat@consensys.net>
2021-02-04 12:08:14 +0100

    feat: pkg register

--- register/pkg/register/register.go
+++ register/pkg/register/register.go
@@ -1,5 +1,9 @@
 package register
 
+import (
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/request"
+)
+
 // GatewayRegister data model
 type GatewayRegister struct {
 	NodeID              string
@@ -21,4 +25,35 @@ type ProviderRegister struct {
 	RegionCode     string
 	RootSigningKey string
 	SigingKey      string
+}
+
+// GetRegisteredGateways returns registered gateways
+func GetRegisteredGateways(registerURL string) ([]GatewayRegister, error) {
+	url := registerURL + "/registers/gateway"
+	gateways := []GatewayRegister{}
+	err := request.GetJSON(url, &gateways)
+	if err != nil {
+		return gateways, err
+	}
+	return gateways, nil
+}
+
+// RegisterProvider to register a provider
+func RegisterProvider(registerURL string, providerRegister ProviderRegister) error {
+	url := registerURL + "/registers/provider"
+	err := request.SendJSON(url, providerRegister)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// RegisterGateway to register a gateway
+func RegisterGateway(registerURL string, gatewayRegister GatewayRegister) error {
+	url := registerURL + "/registers/gateway"
+	err := request.SendJSON(url, gatewayRegister)
+	if err != nil {
+		return err
+	}
+	return nil
 }
