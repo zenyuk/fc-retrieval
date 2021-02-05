feb376decad56055c7f31d8a82a5334a317ed47f
wcgcyx <wcgcyx@gmail.com>
2021-02-05 10:01:56 +1000

    Update nodeid (#65)
    * Fix bugs
    
    * Update nodeid.go
    
    * Node ID fix + testing update

--- pkg/nodeid/nodeid_test.go
+++ pkg/nodeid/nodeid_test.go
@@ -1,4 +1,5 @@
 package nodeid
+
 /*
  * Copyright 2020 ConsenSys Software Inc.
  *
@@ -14,63 +15,59 @@ package nodeid
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
 import (
-    "testing"
-    "encoding/hex"
-    "math/big"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
-
 func Test(t *testing.T) {
-    NewRandomNodeID()
+	NewRandomNodeID()
 }
 
 func TestRoundTripBigInt(t *testing.T) {
-    testRoundTripBigInt(t, "00")
-    testRoundTripBigInt(t, "01")
-    testRoundTripBigInt(t, "fe")
-    testRoundTripBigInt(t, "0100")
-    testRoundTripBigInt(t, "30010203040506070809")
-    testRoundTripBigInt(t, "80010203040506070809")
+	testRoundTripBigInt(t, "00")
+	testRoundTripBigInt(t, "01")
+	testRoundTripBigInt(t, "fe")
+	testRoundTripBigInt(t, "0100")
+	testRoundTripBigInt(t, "30010203040506070809")
+	testRoundTripBigInt(t, "80010203040506070809")
 }
 
 func TestRoundTripFromBytes(t *testing.T) {
-    testRoundTripFromBytes(t, "00")
-    testRoundTripFromBytes(t, "01")
-    testRoundTripFromBytes(t, "fe")
-    testRoundTripFromBytes(t, "0100")
-    testRoundTripFromBytes(t, "30010203040506070809")
-    testRoundTripFromBytes(t, "80010203040506070809")
+	testRoundTripFromBytes(t, "00")
+	testRoundTripFromBytes(t, "01")
+	testRoundTripFromBytes(t, "fe")
+	testRoundTripFromBytes(t, "0100")
+	testRoundTripFromBytes(t, "30010203040506070809")
+	testRoundTripFromBytes(t, "80010203040506070809")
 }
-
-
 
 func TestRoundTripFromString(t *testing.T) {
-    testRoundTripFromString(t, "00")
-    testRoundTripFromString(t, "01")
-    testRoundTripFromString(t, "fe")
-    testRoundTripFromString(t, "0100")
-    testRoundTripFromString(t, "30010203040506070809")
-    testRoundTripFromString(t, "80010203040506070809")
+	testRoundTripFromString(t, "00")
+	testRoundTripFromString(t, "01")
+	testRoundTripFromString(t, "fe")
+	testRoundTripFromString(t, "0100")
+	testRoundTripFromString(t, "30010203040506070809")
+	testRoundTripFromString(t, "80010203040506070809")
 }
 
-
-
 func testRoundTripBigInt(t *testing.T, value string) {
-    id := new(big.Int)
-    _, ok := id.SetString(value, 16)
-    if !ok {
-        panic("Number format issue: " + value)
-    }
-    nodeID, err := NewNodeID(id)
-    if err != nil {
-        panic(err)
-    }
-    idStr := nodeID.ToString()
-    assert.Equal(t, value, idStr, "NewNodeID failed")
+	id := new(big.Int)
+	_, ok := id.SetString(value, 16)
+	if !ok {
+		panic("Number format issue: " + value)
+	}
+
+	nodeID, err := NewNodeID(id)
+	if err != nil {
+		panic(err)
+	}
+	idStr := nodeID.ToString()
+	assert.Equal(t, fmt.Sprintf("%064s", value), idStr, "NewNodeID failed")
 
 }
 
@@ -80,23 +77,21 @@ func testRoundTripFromBytes(t *testing.T, value string) {
 		panic(err)
 	}
 
-    nodeID, err := NewNodeIDFromBytes(bytes)
-    if err != nil {
-        panic(err)
-    }
-    idStr := nodeID.ToString()
-    assert.Equal(t, value, idStr, "NewNodeIDFromString failed")
+	nodeID, err := NewNodeIDFromBytes(bytes)
+	if err != nil {
+		panic(err)
+	}
+	idStr := nodeID.ToString()
+	assert.Equal(t, fmt.Sprintf("%064s", value), idStr, "NewNodeIDFromString failed")
 
 }
 
-
-
 func testRoundTripFromString(t *testing.T, value string) {
-    nodeID, err := NewNodeIDFromString(value)
-    if err != nil {
-        panic(err)
-    }
-    idStr := nodeID.ToString()
-    assert.Equal(t, value, idStr, "NewNodeIDFromString failed")
+	nodeID, err := NewNodeIDFromString(value)
+	if err != nil {
+		panic(err)
+	}
+	idStr := nodeID.ToString()
+	assert.Equal(t, fmt.Sprintf("%064s", value), idStr, "NewNodeIDFromString failed")
 
 }
