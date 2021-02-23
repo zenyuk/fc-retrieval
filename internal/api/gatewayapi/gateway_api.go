655e04b5a0fe8fd63cc0672625c2160ad15bfceb
wcgcyx <wcgcyx@gmail.com>
2021-02-23 13:48:02 +1000

    Add provider APIs

---
+++ internal/api/gatewayapi/gateway_api.go
@@ -0,0 +1,66 @@
+package gatewayapi
+
+import (
+	"net"
+
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrmessages"
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrtcpcomms"
+	log "github.com/ConsenSys/fc-retrieval-gateway/pkg/logging"
+	"github.com/ConsenSys/fc-retrieval-provider/internal/util/settings"
+)
+
+// StartGatewayAPI starts the TCP API as a separate go routine.
+func StartGatewayAPI(settings settings.AppSettings) error {
+	// Start server
+	ln, err := net.Listen("tcp", ":"+settings.BindGatewayAPI)
+	if err != nil {
+		return err
+	}
+	go func(ln net.Listener) {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Error1(err)
+				continue
+			}
+			log.Info("Incoming connection from gateway at :%s", conn.RemoteAddr())
+			go handleIncomingGatewayConnection(conn)
+		}
+	}(ln)
+	log.Info("Listening on %s for connections from Gateways", settings.BindGatewayAPI)
+
+	return nil
+}
+
+func handleIncomingGatewayConnection(conn net.Conn) {
+	// Close connection on exit.
+	defer conn.Close()
+
+	// Loop until error occurs and connection is dropped.
+	for {
+		message, err := fcrtcpcomms.ReadTCPMessage(conn, settings.DefaultTCPInactivityTimeout)
+		if err != nil && !fcrtcpcomms.IsTimeoutError(err) {
+			// Error in tcp communication, drop the connection.
+			log.Error1(err)
+			return
+		}
+		if err == nil {
+			if message.MessageType == fcrmessages.GatewayDHTDiscoverRequestType {
+				err = handleSingleCIDOffersPublishRequest(conn, message)
+				if err != nil && fcrtcpcomms.IsTimeoutError(err) {
+					// Error in tcp communication, drop the connection
+					log.Error1(err)
+					return
+				}
+				continue
+			}
+			// Message is invalid.
+			err = fcrtcpcomms.SendInvalidMessage(conn, settings.DefaultTCPInactivityTimeout)
+			if err != nil && !fcrtcpcomms.IsTimeoutError(err) {
+				// Error in tcp communication, drop the connection.
+				log.Error1(err)
+				return
+			}
+		}
+	}
+}
---
+++ internal/api/providerapi/dht_publish_groupcid.go
@@ -0,0 +1,89 @@
+package providerapi
+
+import (
+	"errors"
+
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/cidoffer"
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrcrypto"
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrmessages"
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrtcpcomms"
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/nodeid"
+	"github.com/ConsenSys/fc-retrieval-provider/internal/core"
+	"github.com/ConsenSys/fc-retrieval-provider/internal/util/settings"
+)
+
+// RequestDHTProviderPublishGroupCID is used to publish a dht group CID offer to a given gateway
+func RequestDHTProviderPublishGroupCID(offers []cidoffer.CidGroupOffer, gatewayID *nodeid.NodeID) error {
+	// Get the core structure
+	c := core.GetSingleInstance()
+
+	// Get the connection to the given gateway
+	gComm, err := c.GatewayCommPool.GetConnForRequestingNode(gatewayID, fcrtcpcomms.AccessFromProvider)
+	if err != nil {
+		return err
+	}
+
+	// Construct message, TODO: Add nonce
+	request, err := fcrmessages.EncodeProviderDHTPublishGroupCIDRequest(1, c.ProviderID, offers)
+	if err != nil {
+		return err
+	}
+	err = fcrtcpcomms.SendTCPMessage(gComm.Conn, request, settings.DefaultTCPInactivityTimeout)
+	if err != nil {
+		c.GatewayCommPool.DeregisterNodeCommunication(gatewayID)
+		return err
+	}
+	// Get a response
+	response, err := fcrtcpcomms.ReadTCPMessage(gComm.Conn, settings.DefaultLongTCPInactivityTimeout)
+	if err != nil {
+		c.GatewayCommPool.DeregisterNodeCommunication(gatewayID)
+		return err
+	}
+	// Need to verify the acks
+	nonce, sig, err := fcrmessages.DecodeProviderDHTPublishGroupCIDAck(response)
+	if err != nil {
+		return err
+	}
+
+	// Check nonce
+	if nonce != 1 {
+		return errors.New("Nonce mismatch")
+	}
+
+	// Check signature
+	// Get the public key
+	c.RegisteredGatewaysMapLock.RLock()
+	defer c.RegisteredGatewaysMapLock.RUnlock()
+	gateway, ok := c.RegisteredGatewaysMap[gatewayID.ToString()]
+	if !ok {
+		return errors.New("Gateway public key not found")
+	}
+	pubKey, err := gateway.GetSigningKey()
+	if err != nil {
+		return errors.New("Fail to get signing key from gateway registration info")
+	}
+
+	ok, err = fcrcrypto.VerifyMessage(pubKey, sig, request)
+	if err != nil {
+		return errors.New("Internal error in verifying ack")
+	}
+
+	if !ok {
+		return errors.New("Fail to verify the ack")
+	}
+
+	for _, offer := range offers {
+		c.AcknowledgementMapLock.Lock()
+		defer c.AcknowledgementMapLock.Unlock()
+		cidMap, ok := c.AcknowledgementMap[(*offer.GetCIDs())[0].ToString()]
+		if !ok {
+			cidMap = make(map[string]core.DHTAcknowledgement)
+			c.AcknowledgementMap[(*offer.GetCIDs())[0].ToString()] = cidMap
+		}
+		cidMap[gatewayID.ToString()] = core.DHTAcknowledgement{
+			Msg:    *request,
+			MsgAck: *response,
+		}
+	}
+	return nil
+}
