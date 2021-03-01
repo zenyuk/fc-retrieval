ac86041ad6334b97af9b7285e69492f0f8906c76
Raphaël Pralat <raphael.pralat@consensys.net>
2021-03-01 15:09:27 +0100

    feat: start main enforcer

---
+++ itest/scripts/enforce-main/enforce-main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
+)
+
+func main() {
+	fmt.Printf("Start enforce main packages:\n\n")
+	os.Exit(1)
+}
