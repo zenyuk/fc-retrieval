8bf2b58d099fd964866f0eea29e4e9523e43f862
ssaidali <samir.saidali@consensys.net>
2021-03-01 15:35:43 +0000

    fix: readd merkle proof in cid info

--- common/pkg/fcrmessages/provideradmin.go
+++ common/pkg/fcrmessages/provideradmin.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/ConsenSys/fc-retrieval-common/pkg/cidoffer"
+	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrmerkletree"
 	"github.com/ConsenSys/fc-retrieval-common/pkg/nodeid"
 )
 
@@ -74,6 +75,7 @@ func EncodeProviderAdminGetGroupCIDResponse(
 				QoS:                  offer.QoS,
 				Signature:            offer.Signature,
 				MerkleRoot:           roots[i],
+				MerkleProof:					fcrmerkletree.FCRMerkleProof{},
 				FundedPaymentChannel: fundedPaymentChannel[i],
 			}
 		}
