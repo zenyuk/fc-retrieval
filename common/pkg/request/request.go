526f1db52763eccbeb0d846b6fccae3da54d680b
Raphaël Pralat <raphael.pralat@consensys.net>
2021-03-04 17:03:50 +0100

    feat: add send message

--- common/pkg/request/request.go
+++ common/pkg/request/request.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"time"
+	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrmessages"
+	log "github.com/ConsenSys/fc-retrieval-common/pkg/logging"
 )
 
 var httpClient = &http.Client{Timeout: 10 * time.Second}
@@ -22,8 +24,8 @@ func GetJSON(url string, target interface{}) error {
 
 // SendJSON request Send JSON
 func SendJSON(url string, data interface{}) error {
-	json, _ := json.Marshal(data)
-	req, err := http.NewRequest("POST", url, bytes.NewReader(json))
+	jsonData, _ := json.Marshal(data)
+	req, err := http.NewRequest("POST", url, bytes.NewReader(jsonData))
 	req.Header.Set("Content-Type", "application/json")
 
 	r, err := httpClient.Do(req)
@@ -32,4 +34,23 @@ func SendJSON(url string, data interface{}) error {
 	}
 	defer r.Body.Close()
 	return nil
+}
+
+// SendJSON request Send JSON
+func SendMessage(url string, message *fcrmessages.FCRMessage) (*fcrmessages.FCRMessage, error) {
+	jsonData, _ := json.Marshal(message)
+	log.Info("Sending JSON: %v to url: %v", string(jsonData), url)
+	contentReader := bytes.NewReader(jsonData)
+	req, err := http.NewRequest("POST", url, contentReader)
+	req.Header.Set("Content-Type", "application/json")
+
+	r, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	var data fcrmessages.FCRMessage
+	json.NewDecoder(r.Body).Decode(&data)
+	return &data, nil
 }
