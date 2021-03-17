2058ef0b3d9652624a06bc793a77f53f53e2c879
ssaidali <samir.saidali@consensys.net>
2021-03-17 22:22:32 +0000

    fix: clean unused settings variables

--- internal/settings/defaults.go
+++ internal/settings/defaults.go
@@ -9,15 +9,6 @@ import (
 )
 
 const (
-	// DefaultGatewayGatewayPort default port for gateway to gatway communications.
-	DefaultGatewayGatewayPort = "9010"
-	// DefaultGatewayProviderPort default port for provider to gateway communications.
-	DefaultGatewayProviderPort = "9011"
-	// DefaultGatewayClientPort default port for client to gateway communications.
-	DefaultGatewayClientPort = "9012"
-	// DefaultGatewayAdminPort is the default gateway admin port
-	DefaultGatewayAdminPort = "9013"
-
 	// DefaultTCPInactivityTimeout is the default TCP timeout
 	DefaultTCPInactivityTimeout = 100 * time.Millisecond
 
