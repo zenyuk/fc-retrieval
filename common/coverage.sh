650d6b575ec5684ee1e07b1623dd74b7d9c986d3
ssaidali <samir.saidali@consensys.net>
2021-04-09 06:13:30 +0000

    fix: bc command not found

--- common/coverage.sh
+++ common/coverage.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+trgt_coverage=${1:-"80"}
+
 go test ./... -coverprofile cover.out
 curr_coverage=$(go tool cover -func cover.out | grep total | awk '{print $3}' | tr -d '%')
-trgt_coverage="80"
 
 echo "Total: $curr_coverage%"
 
-if [ "$(echo $curr_coverage | cut -d'.' -f1)" -ge "$trgt_coverage" ]; then
+if awk 'BEGIN {exit !('$curr_coverage' >= '$trgt_coverage')}'; then
   echo "Unit tests coverage is OK!"
   exit 0
 else
