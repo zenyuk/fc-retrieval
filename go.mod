f66b3dc38b542e6c92ddfc3817c4cff20dc2434a
wcgcyx <wcgcyx@gmail.com>
2021-04-15 13:31:04 +1000

    Simplify code

--- go.mod
+++ go.mod
@@ -3,10 +3,9 @@ module github.com/ConsenSys/fc-retrieval-client
 go 1.15
 
 require (
-	github.com/ConsenSys/fc-retrieval-common v0.0.0-20210331012500-bf32c4270d66
-	github.com/ConsenSys/fc-retrieval-register v0.0.0-20210331012722-2659896d78fb
+	github.com/ConsenSys/fc-retrieval-common v0.0.0-20210415004850-4b547e6f8e3f
+	github.com/ConsenSys/fc-retrieval-register v0.0.0-20210412175300-220bf1ccc474
 	github.com/bitly/go-simplejson v0.5.0
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
-	github.com/kr/pretty v0.2.1 // indirect
 	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e
 )
--- pkg/fcrclient/builder.go
+++ pkg/fcrclient/builder.go
@@ -1,8 +1,19 @@
-package settings
+package fcrclient
 
-// Copyright (C) 2020 ConsenSys Software Inc
-
-// Filecoin Retrieval Client Settings
+/*
+ * Copyright 2020 ConsenSys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 import (
 	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrcrypto"
@@ -10,8 +21,8 @@ import (
 	"github.com/ConsenSys/fc-retrieval-common/pkg/nodeid"
 )
 
-// BuilderImpl holds the library configuration
-type BuilderImpl struct {
+// SettingsBuilder holds the library configuration
+type SettingsBuilder struct {
 	logLevel         string
 	logTarget        string
 	logServiceName   string
@@ -26,8 +37,8 @@ type BuilderImpl struct {
 }
 
 // CreateSettings creates an object with the default settings.
-func CreateSettings() *BuilderImpl {
-	f := BuilderImpl{}
+func CreateSettings() *SettingsBuilder {
+	f := SettingsBuilder{}
 	f.logLevel = defaultLogLevel
 	f.logTarget = defaultLogTarget
 	f.logServiceName = defaultLogServiceName
@@ -37,39 +48,37 @@ func CreateSettings() *BuilderImpl {
 }
 
 // SetLogging sets the log level and target.
-func (f *BuilderImpl) SetLogging(logLevel string, logTarget string, logServiceName string) {
+func (f *SettingsBuilder) SetLogging(logLevel string, logTarget string, logServiceName string) {
 	f.logLevel = logLevel
 	f.logTarget = logTarget
 	f.logServiceName = logServiceName
 }
 
 // SetEstablishmentTTL sets the time to live for the establishment message between client and gateway.
-func (f *BuilderImpl) SetEstablishmentTTL(ttl int64) {
+func (f *SettingsBuilder) SetEstablishmentTTL(ttl int64) {
 	f.establishmentTTL = ttl
 }
 
 // SetRegisterURL sets the register URL.
-func (f *BuilderImpl) SetRegisterURL(url string) {
+func (f *SettingsBuilder) SetRegisterURL(url string) {
 	f.registerURL = url
 }
 
 // SetBlockchainPrivateKey sets the blockchain private key.
-func (f *BuilderImpl) SetBlockchainPrivateKey(bcPkey *fcrcrypto.KeyPair) {
+func (f *SettingsBuilder) SetBlockchainPrivateKey(bcPkey *fcrcrypto.KeyPair) {
 	f.blockchainPrivateKey = bcPkey
 }
 
 // SetRetrievalPrivateKey sets the retrieval private key.
-func (f *BuilderImpl) SetRetrievalPrivateKey(rPkey *fcrcrypto.KeyPair, ver *fcrcrypto.KeyVersion) {
+func (f *SettingsBuilder) SetRetrievalPrivateKey(rPkey *fcrcrypto.KeyPair, ver *fcrcrypto.KeyVersion) {
 	f.retrievalPrivateKey = rPkey
 	f.retrievalPrivateKeyVer = ver
 }
 
 // Build creates a settings object and initialises the logging system.
-func (f *BuilderImpl) Build() *ClientSettings {
+func (f *SettingsBuilder) Build() *ClientSettings {
 
 	logging.Init1(f.logLevel, f.logTarget, f.logServiceName)
-	// logging.SetLogLevel(f.logLevel)
-	// logging.SetLogTarget(f.logTarget)
 
 	g := ClientSettings{}
 	g.establishmentTTL = f.establishmentTTL
