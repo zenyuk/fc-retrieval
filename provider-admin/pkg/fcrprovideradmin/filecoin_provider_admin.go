136e0d132494df09b621cf3bde5db6b211915a90
wcgcyx <wcgcyx@gmail.com>
2021-05-12 13:18:00 +1000

    Revert "Change admin AP"
    This reverts commit 39d7d077ad7a35fbf68410f19fc257af473acce5.

--- provider-admin/pkg/fcrprovideradmin/filecoin_provider_admin.go
+++ provider-admin/pkg/fcrprovideradmin/filecoin_provider_admin.go
@@ -28,15 +28,6 @@ import (
 	"github.com/ConsenSys/fc-retrieval-provider-admin/pkg/api/adminapi"
 )
 
-// providerInfo stores the information of a remote provider instance
-type providerInfo struct {
-	adminAP string
-
-	id      *nodeid.NodeID
-	privKey *fcrcrypto.KeyPair
-	pubKey  *fcrcrypto.KeyPair
-}
-
 // FilecoinRetrievalProviderAdmin is an example implementation using the api,
 // which holds information about the interaction of the Filecoin
 // Retrieval Provider Admin with Filecoin Retrieval Providers.
@@ -44,7 +35,7 @@ type FilecoinRetrievalProviderAdmin struct {
 	Settings ProviderAdminSettings
 
 	// List of providers this admin is in use
-	ActiveProviders     map[string]*providerInfo
+	ActiveProviders     map[string]register.ProviderRegister
 	ActiveProvidersLock sync.RWMutex
 }
 
@@ -52,50 +43,30 @@ type FilecoinRetrievalProviderAdmin struct {
 func NewFilecoinRetrievalProviderAdmin(settings ProviderAdminSettings) *FilecoinRetrievalProviderAdmin {
 	return &FilecoinRetrievalProviderAdmin{
 		Settings:            settings,
-		ActiveProviders:     make(map[string]*providerInfo),
+		ActiveProviders:     make(map[string]register.ProviderRegister),
 		ActiveProvidersLock: sync.RWMutex{},
 	}
 }
 
 // InitialiseProvider initialise a given provider
-func (c *FilecoinRetrievalProviderAdmin) InitialiseProvider(adminAP string, nodeID *nodeid.NodeID, providerPrivKey *fcrcrypto.KeyPair, providerPrivKeyVer *fcrcrypto.KeyVersion) error {
-	err := adminapi.RequestInitialiseKey(adminAP, nodeID, providerPrivKey, providerPrivKeyVer, c.Settings.providerAdminPrivateKey, c.Settings.providerAdminPrivateKeyVer)
+func (c *FilecoinRetrievalProviderAdmin) InitialiseProvider(providerInfo *register.ProviderRegister, providerPrivKey *fcrcrypto.KeyPair, providerPrivKeyVer *fcrcrypto.KeyVersion) error {
+	err := adminapi.RequestInitialiseKey(providerInfo, providerPrivKey, providerPrivKeyVer, c.Settings.providerAdminPrivateKey, c.Settings.providerAdminPrivateKeyVer)
 	if err != nil {
 		return err
 	}
 
-	// Get pub key
-	encoded, err := providerPrivKey.EncodePublicKey()
+	// Register this provider
+	err = providerInfo.RegisterProvider(c.Settings.RegisterURL())
 	if err != nil {
-		logging.Error("Error in encoding public key")
-		return err
-	}
-	pubKey, err := fcrcrypto.DecodePublicKey(encoded)
-	if err != nil {
-		logging.Error("Error in generating signing key.")
+		logging.Error("Error in register the provider.")
 		return err
 	}
 
 	// Add this provider to the active providers list
 	c.ActiveProvidersLock.Lock()
-	c.ActiveProviders[nodeID.ToString()] = &providerInfo{
-		adminAP: adminAP,
-		id:      nodeID,
-		privKey: providerPrivKey,
-		pubKey:  pubKey,
-	}
+	c.ActiveProviders[providerInfo.NodeID] = *providerInfo
 	c.ActiveProvidersLock.Unlock()
 	return nil
-}
-
-// RegisterProvider registers the given providerInfo to the register
-func (c *FilecoinRetrievalProviderAdmin) RegisterProvider(providerInfo *register.ProviderRegister) error {
-	// Register this provider
-	err := providerInfo.RegisterProvider(c.Settings.RegisterURL())
-	if err != nil {
-		logging.Error("Error in register the provider.")
-	}
-	return err
 }
 
 // PublishGroupCID publish a group cid offer to a given provider
@@ -106,7 +77,7 @@ func (c *FilecoinRetrievalProviderAdmin) PublishGroupCID(providerID *nodeid.Node
 	if !exists {
 		return errors.New("Unable to find the provider in admin storage")
 	}
-	return adminapi.RequestPublishGroupOffer(providerInfo.adminAP, providerInfo.pubKey, cids, price, expiry, qos, c.Settings.providerAdminPrivateKey, c.Settings.providerAdminPrivateKeyVer)
+	return adminapi.RequestPublishGroupOffer(&providerInfo, cids, price, expiry, qos, c.Settings.providerAdminPrivateKey, c.Settings.providerAdminPrivateKeyVer)
 }
 
 // PublishDHTCID publish a dht cid offer to a given provider
@@ -117,7 +88,7 @@ func (c *FilecoinRetrievalProviderAdmin) PublishDHTCID(providerID *nodeid.NodeID
 	if !exists {
 		return errors.New("Unable to find the provider in admin storage")
 	}
-	return adminapi.RequestPublishDHTOffer(providerInfo.adminAP, providerInfo.pubKey, cids, price, expiry, qos, c.Settings.providerAdminPrivateKey, c.Settings.providerAdminPrivateKeyVer)
+	return adminapi.RequestPublishDHTOffer(&providerInfo, cids, price, expiry, qos, c.Settings.providerAdminPrivateKey, c.Settings.providerAdminPrivateKeyVer)
 }
 
 // GetGroupCIDOffer checks the group offer stored in the provider
@@ -128,7 +99,7 @@ func (c *FilecoinRetrievalProviderAdmin) GetGroupCIDOffer(providerID *nodeid.Nod
 	if !exists {
 		return false, nil, errors.New("Unable to find the provider in admin storage")
 	}
-	return adminapi.RequestGetPublishedOffer(providerInfo.adminAP, providerInfo.pubKey, gatewayIDs, c.Settings.providerAdminPrivateKey, c.Settings.providerAdminPrivateKeyVer)
+	return adminapi.RequestGetPublishedOffer(&providerInfo, gatewayIDs, c.Settings.providerAdminPrivateKey, c.Settings.providerAdminPrivateKeyVer)
 }
 
 // ForceUpdate forces the provider to update its internal register
@@ -139,5 +110,5 @@ func (c *FilecoinRetrievalProviderAdmin) ForceUpdate(providerID *nodeid.NodeID) 
 	if !exists {
 		return errors.New("Unable to find the provider in admin storage")
 	}
-	return adminapi.RequestForceRefresh(providerInfo.adminAP, providerInfo.pubKey, c.Settings.providerAdminPrivateKey, c.Settings.providerAdminPrivateKeyVer)
+	return adminapi.RequestForceRefresh(&providerInfo, c.Settings.providerAdminPrivateKey, c.Settings.providerAdminPrivateKeyVer)
 }
