3dc258cdc7272fda1ce73befa92f815c8b2b9883
Igor Zenyuk <igor.zenyuk@gmail.com>
2021-05-18 15:50:10 +1000

    225-test-containers-cleanup

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -10,7 +10,6 @@ import (
 	"strings"
 
 	"github.com/docker/docker/api/types/container"
-	"github.com/google/uuid"
 	"github.com/testcontainers/testcontainers-go/wait"
 	tc "github.com/wcgcyx/testcontainers-go"
 )
@@ -30,34 +29,6 @@ const ColorBrightBlue = "\033[91m"
 const ColorBrightPurple = "\033[91m"
 const ColorBrightCyan = "\033[91m"
 const ColorBrightWhite = "\033[91m"
-
-func StartContainers() (string, error) {
-	composeFilePaths := []string{"../../docker-compose.yml"}
-	identifier := strings.ToLower(uuid.New().String())
-
-	compose := tc.NewLocalDockerCompose(composeFilePaths, identifier)
-	execError := compose.
-		WithCommand([]string{"up", "-d"}).
-		Invoke()
-
-	err := execError.Error
-	if err != nil {
-		return "", fmt.Errorf("could not run compose file: %v - %v", composeFilePaths, err)
-	}
-	return compose.Identifier, nil
-}
-
-func StopContainers(composeID string) error {
-	composeFilePaths := []string{"../../docker-compose.yml"}
-
-	compose := tc.NewLocalDockerCompose(composeFilePaths, composeID)
-	execError := compose.Down()
-	err := execError.Error
-	if err != nil {
-		return fmt.Errorf("could not stop compose file: %v - %v", composeFilePaths, err)
-	}
-	return nil
-}
 
 // CleanContainers clean the containers
 func CleanContainers(network string) {
