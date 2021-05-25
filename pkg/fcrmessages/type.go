39399bbeff8350974d6071410a9fd093832b6f19
James Fan <james.fan@consensys.net>
2021-05-25 21:55:33 +1000

    FSR-253 add message ClientStandardDiscoverResponseV2

--- pkg/fcrmessages/type.go
+++ pkg/fcrmessages/type.go
@@ -26,6 +26,7 @@ const (
 	ClientDHTDiscoverResponseType      = 105
 	ClientDHTOfferAckRequestType       = 106
 	ClientDHTOfferAckResponseType      = 107
+	ClientStandardDiscoverResponseV2Type = 108
 
 	// Message originating from gateway
 	GatewayListDHTOfferRequestType                          = 200
