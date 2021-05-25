22713a91d865b200995c4bec955ce6a1efe729cf
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-05-25 16:37:59 +0100

    FSR-254 - Add ClientStandardDiscoverOfferRequest

--- pkg/fcrmessages/type.go
+++ pkg/fcrmessages/type.go
@@ -18,14 +18,15 @@ package fcrmessages
 // Message types
 const (
 	// Message originating from client
-	ClientEstablishmentRequestType     = 100
-	ClientEstablishmentResponseType    = 101
-	ClientStandardDiscoverRequestType  = 102
-	ClientStandardDiscoverResponseType = 103
-	ClientDHTDiscoverRequestType       = 104
-	ClientDHTDiscoverResponseType      = 105
-	ClientDHTOfferAckRequestType       = 106
-	ClientDHTOfferAckResponseType      = 107
+	ClientEstablishmentRequestType         = 100
+	ClientEstablishmentResponseType        = 101
+	ClientStandardDiscoverRequestType      = 102
+	ClientStandardDiscoverResponseType     = 103
+	ClientDHTDiscoverRequestType           = 104
+	ClientDHTDiscoverResponseType          = 105
+	ClientDHTOfferAckRequestType           = 106
+	ClientDHTOfferAckResponseType          = 107
+	ClientStandardDiscoverOfferRequestType = 110
 
 	// Message originating from gateway
 	GatewayListDHTOfferRequestType                          = 200
