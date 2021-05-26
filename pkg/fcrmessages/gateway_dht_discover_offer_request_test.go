e0fa5c32f55e2287647c93d601b6495372d5512c
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-05-26 09:30:16 +0100

    Update namming to OfferDigests

--- pkg/fcrmessages/gateway_dht_discover_offer_request_test.go
+++ pkg/fcrmessages/gateway_dht_discover_offer_request_test.go
@@ -13,21 +13,21 @@ func TestEncodeGatewayDHTDiscoverOfferRequest(t *testing.T) {
 	mockContentID, _ := cid.NewContentIDFromBytes([]byte{1})
 	mockNonce := int64(42)
 
-	mockOffersDigest := [][cidoffer.CIDOfferDigestSize]byte{{1, 2, 4}, {5, 6, 7}}
+	mockOfferDigests := [][cidoffer.CIDOfferDigestSize]byte{{1, 2, 4}, {5, 6, 7}}
 	mockPayChAddr := "t2twbvr2oaxqzyktxqqjrv37bh7gzfhuqonfioayq"
 	mockVoucher := "i1UCnYNY6cC8M4VO8IJjXfwn-bJT0g4AAED2AAFJAEVjkYJE9AAAAIBYYQK3pJLhIR8XTVSmQzsEiE7NIId2-2DPbWF396mBPBJCdoSQ_ctibPesW-YMnzKhGAEScF09H_sldF1nTfizTbsjWea9MN6R3T0Ew0Lb4znHtJnucGAkcbdlIyDAHCScOXE"
 	validMsg := &FCRMessage{
 		messageType:       209,
 		protocolVersion:   1,
 		protocolSupported: []int32{1, 1},
-		messageBody:       []byte(`{"piece_cid":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAE=","nonce":42,"offers_digest":[[1,2,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[5,6,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],"payment_channel_address":"t2twbvr2oaxqzyktxqqjrv37bh7gzfhuqonfioayq","voucher":"i1UCnYNY6cC8M4VO8IJjXfwn-bJT0g4AAED2AAFJAEVjkYJE9AAAAIBYYQK3pJLhIR8XTVSmQzsEiE7NIId2-2DPbWF396mBPBJCdoSQ_ctibPesW-YMnzKhGAEScF09H_sldF1nTfizTbsjWea9MN6R3T0Ew0Lb4znHtJnucGAkcbdlIyDAHCScOXE"}`),
+		messageBody:       []byte(`{"piece_cid":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAE=","nonce":42,"offer_digests":[[1,2,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[5,6,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],"payment_channel_address":"t2twbvr2oaxqzyktxqqjrv37bh7gzfhuqonfioayq","voucher":"i1UCnYNY6cC8M4VO8IJjXfwn-bJT0g4AAED2AAFJAEVjkYJE9AAAAIBYYQK3pJLhIR8XTVSmQzsEiE7NIId2-2DPbWF396mBPBJCdoSQ_ctibPesW-YMnzKhGAEScF09H_sldF1nTfizTbsjWea9MN6R3T0Ew0Lb4znHtJnucGAkcbdlIyDAHCScOXE"}`),
 		signature:         "",
 	}
 
 	msg, err := EncodeGatewayDHTDiscoverOfferRequest(
 		mockContentID,
 		mockNonce,
-		mockOffersDigest,
+		mockOfferDigests,
 		mockPayChAddr,
 		mockVoucher,
 	)
@@ -40,14 +40,14 @@ func TestEncodeGatewayDHTDiscoverOfferRequest(t *testing.T) {
 func TestDecodeGatewayDHTDiscoverOfferRequest(t *testing.T) {
 	mockContentID, _ := cid.NewContentIDFromBytes([]byte{1})
 	mockNonce := int64(42)
-	mockOfferDigest := [][cidoffer.CIDOfferDigestSize]byte{{1, 2, 4}, {5, 6, 7}}
+	mockOfferDigests := [][cidoffer.CIDOfferDigestSize]byte{{1, 2, 4}, {5, 6, 7}}
 	mockPayChAddr := "t2twbvr2oaxqzyktxqqjrv37bh7gzfhuqonfioayq"
 	mockVoucher := "i1UCnYNY6cC8M4VO8IJjXfwn-bJT0g4AAED2AAFJAEVjkYJE9AAAAIBYYQK3pJLhIR8XTVSmQzsEiE7NIId2-2DPbWF396mBPBJCdoSQ_ctibPesW-YMnzKhGAEScF09H_sldF1nTfizTbsjWea9MN6R3T0Ew0Lb4znHtJnucGAkcbdlIyDAHCScOXE"
 	validMsg := &FCRMessage{
 		messageType:       209,
 		protocolVersion:   1,
 		protocolSupported: []int32{1, 1},
-		messageBody:       []byte(`{"piece_cid":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAE=","nonce":42,"offers_digest":[[1,2,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[5,6,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],"payment_channel_address":"t2twbvr2oaxqzyktxqqjrv37bh7gzfhuqonfioayq","voucher":"i1UCnYNY6cC8M4VO8IJjXfwn-bJT0g4AAED2AAFJAEVjkYJE9AAAAIBYYQK3pJLhIR8XTVSmQzsEiE7NIId2-2DPbWF396mBPBJCdoSQ_ctibPesW-YMnzKhGAEScF09H_sldF1nTfizTbsjWea9MN6R3T0Ew0Lb4znHtJnucGAkcbdlIyDAHCScOXE"}`),
+		messageBody:       []byte(`{"piece_cid":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAE=","nonce":42,"offer_digests":[[1,2,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[5,6,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],"payment_channel_address":"t2twbvr2oaxqzyktxqqjrv37bh7gzfhuqonfioayq","voucher":"i1UCnYNY6cC8M4VO8IJjXfwn-bJT0g4AAED2AAFJAEVjkYJE9AAAAIBYYQK3pJLhIR8XTVSmQzsEiE7NIId2-2DPbWF396mBPBJCdoSQ_ctibPesW-YMnzKhGAEScF09H_sldF1nTfizTbsjWea9MN6R3T0Ew0Lb4znHtJnucGAkcbdlIyDAHCScOXE"}`),
 		signature:         "",
 	}
 
@@ -55,7 +55,7 @@ func TestDecodeGatewayDHTDiscoverOfferRequest(t *testing.T) {
 	assert.Empty(t, err)
 	assert.Equal(t, pieceCID, mockContentID)
 	assert.Equal(t, nonce, mockNonce)
-	assert.Equal(t, offerDigest, mockOfferDigest)
+	assert.Equal(t, offerDigest, mockOfferDigests)
 	assert.Equal(t, paychAddr, mockPayChAddr)
 	assert.Equal(t, voucher, mockVoucher)
 }
