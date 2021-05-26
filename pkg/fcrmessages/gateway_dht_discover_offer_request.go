9bd2c56bef8e2c805b43f0cadbecef69e58280ae
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-05-26 07:47:40 +0100

    FSR-257 update GatewayDHTDiscoverOfferRequest

--- pkg/fcrmessages/gateway_dht_discover_offer_request.go
+++ pkg/fcrmessages/gateway_dht_discover_offer_request.go
@@ -25,30 +25,27 @@ import (
 
 // gatewayDHTDiscoverOfferRequest is the request to implement the payment system
 type gatewayDHTDiscoverOfferRequest struct {
-	PieceCID    cid.ContentID                     `json:"piece_cid"`
-	Nonce       int64                             `json:"nonce"`
-	TTL         int64                             `json:"ttl"`
-	OfferDigest [cidoffer.CIDOfferDigestSize]byte `json:"offer_digest"`
-	PaychAddr   string                            `json:"payment_channel_address"`
-	Voucher     string                            `json:"voucher"`
+	PieceCID     cid.ContentID                       `json:"piece_cid"`
+	Nonce        int64                               `json:"nonce"`
+	OfferDigests [][cidoffer.CIDOfferDigestSize]byte `json:"offer_digest"`
+	PaychAddr    string                              `json:"payment_channel_address"`
+	Voucher      string                              `json:"voucher"`
 }
 
 // EncodeGatewayDHTDiscoverOfferRequest is used to get the FCRMessage of gatewayDHTDiscoverOfferRequest
 func EncodeGatewayDHTDiscoverOfferRequest(
 	pieceCID *cid.ContentID,
 	nonce int64,
-	ttl int64,
-	offerDigest [cidoffer.CIDOfferDigestSize]byte,
+	offerDigest [][cidoffer.CIDOfferDigestSize]byte,
 	paychAddr string,
 	voucher string,
 ) (*FCRMessage, error) {
 	body, err := json.Marshal(gatewayDHTDiscoverOfferRequest{
-		PieceCID:    *pieceCID,
-		Nonce:       nonce,
-		TTL:         ttl,
-		OfferDigest: offerDigest,
-		PaychAddr:   paychAddr,
-		Voucher:     voucher,
+		PieceCID:     *pieceCID,
+		Nonce:        nonce,
+		OfferDigests: offerDigest,
+		PaychAddr:    paychAddr,
+		Voucher:      voucher,
 	})
 	if err != nil {
 		return nil, err
@@ -60,19 +57,18 @@ func EncodeGatewayDHTDiscoverOfferRequest(
 func DecodeGatewayDHTDiscoverOfferRequest(fcrMsg *FCRMessage) (
 	*cid.ContentID, // piece cid
 	int64, // nonce
-	int64, // ttl
-	[cidoffer.CIDOfferDigestSize]byte, // offer_digest
+	[][cidoffer.CIDOfferDigestSize]byte, // offer_digest
 	string, // payment channel address
 	string, // voucher
 	error, // error
 ) {
 	if fcrMsg.GetMessageType() != GatewayDHTDiscoverOfferRequestType {
-		return nil, 0, 0, [cidoffer.CIDOfferDigestSize]byte{}, "", "", errors.New("Message type mismatch")
+		return nil, 0, [][cidoffer.CIDOfferDigestSize]byte{}, "", "", errors.New("Message type mismatch")
 	}
 	msg := gatewayDHTDiscoverOfferRequest{}
 	err := json.Unmarshal(fcrMsg.GetMessageBody(), &msg)
 	if err != nil {
-		return nil, 0, 0, [cidoffer.CIDOfferDigestSize]byte{}, "", "", err
+		return nil, 0, [][cidoffer.CIDOfferDigestSize]byte{}, "", "", err
 	}
-	return &msg.PieceCID, msg.Nonce, msg.TTL, msg.OfferDigest, msg.PaychAddr, msg.Voucher, nil
+	return &msg.PieceCID, msg.Nonce, msg.OfferDigests, msg.PaychAddr, msg.Voucher, nil
 }
