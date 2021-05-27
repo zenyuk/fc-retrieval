d4ab2d407c9239280b1be12ba954b6f2c9fed8da
James Fan <james.fan@consensys.net>
2021-05-27 11:28:45 +1000

    FSR-247 Gateway paymentMgr initialisation, tidy up

--- internal/core/core.go
+++ internal/core/core.go
@@ -23,13 +23,13 @@ import (
 	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrmerkletree"
 	"github.com/ConsenSys/fc-retrieval-common/pkg/fcroffermgr"
 	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrp2pserver"
+	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrpaymentmgr"
 	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrregistermgr"
 	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrrestserver"
 	"github.com/ConsenSys/fc-retrieval-common/pkg/logging"
 	"github.com/ConsenSys/fc-retrieval-common/pkg/nodeid"
 	"github.com/ConsenSys/fc-retrieval-gateway/internal/reputation"
 	"github.com/ConsenSys/fc-retrieval-gateway/internal/util/settings"
-	"github.com/ConsenSys/fc-retrieval-common/pkg/fcrpaymentmgr"
 )
 
 const (
