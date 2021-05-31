8d45a8bbd8829533c53cea2d42342fe86b014565
Raphael Pralat <raphael.pralat@consensys.net>
2021-05-31 23:09:24 +0200

    feat: test ok

--- src/Client.test.ts
+++ src/Client.test.ts
@@ -1,9 +1,44 @@
 import { Client } from './index'
 import { defaults } from './defaults'
+import axios, { AxiosResponse } from 'axios'
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+const mockedResponse: AxiosResponse = {
+  data: [
+    {
+      nodeID: '9876543210',
+      address: 'f01234',
+      networkInfoAdmin: '127.0.0.1:80',
+      networkInfoClient: '127.0.0.1:80',
+      networkInfoGateway: '127.0.0.1:80',
+      networkInfoProvider: '127.0.0.1:80',
+      regionCode: 'FR',
+      rootSigningKey: '0xABCDE123456789',
+      signingKey: '0x987654321EDCBA',
+    },
+  ],
+  status: 200,
+  statusText: 'OK',
+  headers: {},
+  config: {},
+}
 
 describe('Client', () => {
-  it('Hello', () => {
+  // it('findGateways', async () => {
+  //   const client = new Client(defaults)
+  //   mockedAxios.get.mockResolvedValue(mockedResponse)
+
+  //   const gateways = await client.findGateways()
+  //   expect(gateways).toEqual(mockedResponse.data)
+  // })
+
+  it('addGatewaysToUse', async () => {
     const client = new Client(defaults)
-    expect(client.findGateways()).toBe('Hello World')
+    // mockedAxios.get.mockResolvedValue(mockedResponse)
+
+    await client.addGatewaysToUse(['9876543210'])
+    const gateways = await client.getGatewaysToUse()
+    expect(gateways.nodeID).toEqual({ '9876543210': mockedResponse.data[0].nodeID })
   })
 })
--- src/Client.ts
+++ src/Client.ts
@@ -1,22 +1,26 @@
+import axios from 'axios'
+
 import { defaults, Settings } from './defaults'
+import { FCRPaymentMgr } from './data/types'
+import { GatewayRegister, getGatewayByID } from './Register'
 
 export interface Gateway {
+  nodeID: string
   address: string
   networkInfoAdmin: string
   networkInfoClient: string
   networkInfoGateway: string
   networkInfoProvider: string
-  nodeId: string
   regionCode: string
   rootSigningKey: string
-  sigingKey: string
+  signingKey: string
 }
 
 export class CreateSettings {
   settings: Settings
 
   constructor() {
-    this.settings = defaults
+    this.settings = defaults as Settings
   }
 
   setDefaultEstablishmentTTL(ttl: number) {
@@ -44,26 +48,82 @@ export class CreateSettings {
   }
 }
 
+export interface GatewaysToUse {
+  [index: string]: GatewayRegister
+}
+
 export class Client {
   settings: Settings
-  gateways: Gateway[]
+  gatewaysToUse: GatewaysToUse
+  paymentMgrLock: FCRPaymentMgr[]
 
   constructor(settings: Settings) {
     this.settings = settings
-    this.gateways = []
+    this.gatewaysToUse = {} as GatewaysToUse
+    this.paymentMgrLock = [] as FCRPaymentMgr[]
   }
 
+  paymentMgr() {}
+
+  // FindGateways find gateways located near to the specified location. Use AddGateways
+  // to use these gateways.
   async findGateways(location: string = '', maxNumToLocate: number = 16) {
+    const url = this.settings.defaultRegisterURL + '/registers/gateway'
     try {
-      const response = await fetch(this.settings.defaultRegisterURL + '/registers/gateway')
-      this.gateways = await response.json()
-      return this.gateways
+      const response = await axios.get(url)
+      const gateways = response.data as GatewayRegister[]
+      return gateways
     } catch (error) {
-      throw new Error('Fail to fetch data')
+      throw new Error('Fail to fetch data: ' + url)
     }
   }
 
-  async connectedGateways() {
-    return this.gateways
+  // AddGatewaysToUse adds one or more gateways to use.
+  addGatewaysToUse(gwNodeIDs: string[]) {
+    const numAdded: number = 0
+    for (const nodeID of gwNodeIDs) {
+      const gateway = getGatewayByID(nodeID)
+      this.gatewaysToUse[nodeID] = gateway
+    }
   }
+
+  // RemoveGatewaysToUse removes one or more gateways from the list of Gateways to use.
+  // This also removes the gateway from gateways in active map.
+  removeGatewaysToUse() {}
+
+  // RemoveAllGatewaysToUse removes all gateways from the list of Gateways.
+  // This also cleared all gateways in active
+  removeAllGatewaysToUse() {}
+
+  // GetGatewaysToUse returns the list of gateways to use.
+  getGatewaysToUse() {
+    return this.gatewaysToUse
+  }
+
+  // AddActiveGateways adds one or more gateways to active gateway map.
+  // Returns the number of gateways added.
+  addActiveGateways() {}
+
+  // RemoveActiveGateways removes one or more gateways from the list of Gateways in active.
+  removeActiveGateways() {}
+
+  // RemoveAllActiveGateways removes all gateways from the list of Gateways in active.
+  removeAllActiveGateways() {}
+
+  // GetActiveGateways returns the list of gateways that are active.
+  getActiveGateways() {}
+
+  // FindOffersStandardDiscovery finds offer using standard discovery from given gateways
+  findOffersStandardDiscovery() {}
+
+  // FindOffersDHTDiscovery finds offer using dht discovery from given gateways
+  findOffersDHTDiscovery() {}
+
+  // FindDHTOfferAck finds offer ack for a cid, gateway pair
+  findDHTOfferAck() {}
+
+  // FindOffersStandardDiscoveryV2 finds offer using standard discovery from given gateways
+  findOffersStandardDiscoveryV2() {}
+
+  private pay() {}
 }
