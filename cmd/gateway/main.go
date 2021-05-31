59fb6205c830cb53bf1dbfd2d0aabc80c7d6339d
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-05-31 14:48:57 +0100

    FSR-251 - Gateway receives payment to respond to gateway DHT discovery

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -87,6 +87,7 @@ func main() {
 	c.P2PServer.
 		// gateway api
 		AddHandler(appSettings.BindGatewayAPI, fcrmessages.GatewayDHTDiscoverRequestType, gatewayapi.HandleGatewayDHTDiscoverRequest).
+		AddHandler(appSettings.BindGatewayAPI, fcrmessages.GatewayDHTDiscoverRequestV2Type, gatewayapi.HandleGatewayDHTDiscoverRequestV2).
 		AddRequester(fcrmessages.GatewayDHTDiscoverRequestType, gatewayapi.RequestGatewayDHTDiscover).
 		AddRequester(fcrmessages.GatewayDHTDiscoverRequestV2Type, gatewayapi.RequestGatewayDHTDiscoverV2).
 		AddRequester(fcrmessages.GatewayListDHTOfferRequestType, gatewayapi.RequestListCIDOffer).
