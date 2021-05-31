ad74d28c8e503dc142bb3c521803713318cd09ca
Raphael Pralat <raphael.pralat@consensys.net>
2021-05-31 23:09:24 +0200

    feat: test ok

---
+++ client-js/src/data/types.ts
@@ -0,0 +1,8 @@
+// Payment manager manages all payment related functions
+export type FCRPaymentMgr = {
+  privKey: string
+  address: string
+  authToken: string
+  lotusAPIAddr: string
+}
+export type toto = {}
