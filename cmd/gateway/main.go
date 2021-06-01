509be9db434ee3b034cc11c88f879f3ceb732eea
James Fan <james.fan@consensys.net>
2021-06-02 09:01:00 +1000

    FSR-271 - Gateway receives payment to respond to client standard offer query

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -61,6 +61,7 @@ func main() {
 		AddHandler(appSettings.BindRestAPI, fcrmessages.ClientEstablishmentRequestType, clientapi.HandleClientEstablishmentRequest).
 		AddHandler(appSettings.BindRestAPI, fcrmessages.ClientDHTDiscoverRequestType, clientapi.HandleClientDHTCIDDiscoverRequest).
 		AddHandler(appSettings.BindRestAPI, fcrmessages.ClientStandardDiscoverRequestType, clientapi.HandleClientStandardCIDDiscoverRequest).
+		AddHandler(appSettings.BindRestAPI, fcrmessages.ClientStandardDiscoverOfferRequestType, clientapi.HandleClientStandardDiscoverOfferRequest).
 		// admin api
 		AddHandler(appSettings.BindAdminAPI, fcrmessages.GatewayAdminInitialiseKeyRequestType, adminapi.HandleGatewayAdminInitialiseKeyRequest).
 		AddHandler(appSettings.BindAdminAPI, fcrmessages.GatewayAdminInitialiseKeyRequestV2Type, adminapi.HandleGatewayAdminInitialiseKeyRequestV2).
