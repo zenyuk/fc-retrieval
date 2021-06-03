64f0575a304b0c1fdc650a0026ad8584909b6ef0
Samir <samir.saidali@consensys.net>
2021-06-03 15:40:24 +0200

    feat: add fcrMerkleTree class

---
+++ client-js/src/fcrMerkleTree/tree.class.ts
@@ -0,0 +1,21 @@
+import {Content, MerkleTree} from './types';
+import { FCRMerkleProof } from './proof.class';
+
+export class FCRMerkleTree {
+	tree: MerkleTree;
+
+  getMerkleRoot(): string {
+    // TODO
+    return '';
+  }
+
+  generateMerkleProof(content: Content): FCRMerkleProof {
+    // TODO
+    return undefined;
+  }
+}
+
+export const createMerkleTree = (contents: Content[]): FCRMerkleTree => {
+	// TODO
+	return undefined;
+}
