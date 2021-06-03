e0095853b2a4007b7b090e453831c8c469f0d66b
wcgcyx <wcgcyx@gmail.com>
2021-06-04 03:41:36 +1000

    Fix lotus issue

--- lotus/lotus-base/setup-lotus-base.sh
+++ lotus/lotus-base/setup-lotus-base.sh
@@ -5,4 +5,13 @@ set -m
 sleep 30
 ./lotus wallet import --as-default ~/.genesis-sectors/pre-seal-t01000.key
 ./lotus-miner init --genesis-miner --actor=t01000 --sector-size=2KiB --pre-sealed-sectors=~/.genesis-sectors --pre-sealed-metadata=~/.genesis-sectors/pre-seal-t01000.json
+./lotus-miner run --nosync &
+height=$(./lotus chain getblock $(./lotus chain head) | jq -r ".Height")
+while [ $height -lt 40 ]
+do
+	height=$(./lotus chain getblock $(./lotus chain head) | jq -r ".Height")
+    echo $height
+	sleep 5
+done
+./lotus-miner stop
 ./lotus daemon stop
