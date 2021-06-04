30d9212d296826605a02d47f89600ab710aa6d9c
Raphael Pralat <raphael.pralat@consensys.net>
2021-06-04 11:32:15 +0200

    feat: request

--- src/clientapi/standard_discover_requester_v2.ts
+++ src/clientapi/standard_discover_requester_v2.ts
@@ -1,5 +1,6 @@
 import { ContentID } from '../cid/cid.interface'
 import { GatewayRegister } from '../register/register.class'
+import { sendMessage } from '../request/request'
 
 // RequestStandardDiscoverV2 requests a standard discover to a given gateway for a given contentID, nonce and ttl.
 export const requestStandardDiscoverV2 = (
@@ -10,6 +11,10 @@ export const requestStandardDiscoverV2 = (
   paychAddr: string,
   voucher: string,
 ): string[] => {
+  const resp = sendMessage(gatewayInfo.networkInfoClient, request)
+
+  //gatewayInfo.signingKey
+
   //
   const offerDigests = [] as string[]
   return offerDigests
