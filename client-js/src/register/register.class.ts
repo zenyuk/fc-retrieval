2e96c80a5841aef5bfb7c45727d7b9d9542c7107
Raphael Pralat <raphael.pralat@consensys.net>
2021-06-04 17:01:34 +0200

    feat: request

--- client-js/src/register/register.class.ts
+++ client-js/src/register/register.class.ts
@@ -1,3 +1,5 @@
+import { NodeID } from '../nodeid/nodeid.interface'
+
 export class Register {}
 
 export class GatewayRegister {
@@ -16,19 +18,38 @@ export class GatewayRegister {
   }
 }
 
-export interface ProviderRegister {
-  nodeID: string
-  address: string
-  rootSigningKey: string
-  signingKey: string
-  regionCode: string
-  networkInfoGateway: string
-  networkInfoProvider: string
-  networkInfoClient: string
-  networkInfoAdmin: string
+export class ProviderRegister {
+  nodeID: string = ''
+  address: string = ''
+  rootSigningKey: string = ''
+  signingKey: string = ''
+  regionCode: string = ''
+  networkInfoGateway: string = ''
+  networkInfoProvider: string = ''
+  networkInfoClient: string = ''
+  networkInfoAdmin: string = ''
+
+  getSigningKey(): string {
+    return ''
+  }
 }
 
-export const getProviderByID = (registerURL: string, nodeID: string): ProviderRegister => {
+export const getGatewayByID = (registerURL: string, nodeID: NodeID): ProviderRegister => {
   //
   return {} as ProviderRegister
+}
+
+export const getProviderByID = (registerURL: string, nodeID: NodeID): ProviderRegister => {
+  //
+  return {} as ProviderRegister
+}
+
+export const validateProviderInfo = (register: ProviderRegister): boolean => {
+  //
+  return {} as boolean
+}
+
+export const validateGatewayInfo = (register: GatewayRegister): boolean => {
+  //
+  return {} as boolean
 }
