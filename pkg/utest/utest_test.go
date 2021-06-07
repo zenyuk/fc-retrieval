3eec1454bf08313108667c09c526cc4630ce409d
James Fan <james.fan@consensys.net>
2021-06-07 14:29:46 +1000

    FSR-182 - Add utest package for common unit test funcations and interfaces

---
+++ pkg/utest/utest_test.go
@@ -0,0 +1,49 @@
+package utest
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func ExampleNewTestClientString() {
+	c := NewTestClientString("OK")
+	fmt.Printf("%T\n", c)
+
+	r, err := c.Get("")
+	fmt.Println(r.Body)
+	fmt.Println(err)
+	// Output:
+	// *http.Client
+	// {OK}
+	// <nil>
+}
+
+func ExampleNewTestClientError() {
+	c := NewTestClientError()
+	fmt.Printf("%T\n", c)
+
+	r, err := c.Get("")
+	fmt.Println(err)
+
+	bytes, err := ioutil.ReadAll(r.Body)
+	fmt.Println(bytes)
+	fmt.Println(err)
+	// Output:
+	// *http.Client
+	// <nil>
+	// []
+	// test error
+}
+
+func ExampleNewTestClient() {
+	fn := func(req *http.Request) *http.Response {
+		return &http.Response{}
+	}
+	c := NewTestClient(fn)
+	fmt.Printf("%T\n", c)
+	fmt.Printf("%T\n", c.Transport)
+	// Output:
+	// *http.Client
+	// utest.RoundTripFunc
+}
