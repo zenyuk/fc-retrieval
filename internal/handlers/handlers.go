e2f9bd4a6183ede6bcbbfb8bf35af0412faa39ca
zenyuk <33681156+zenyuk@users.noreply.github.com>
2021-06-07 15:19:59 +1000

    282 write godoc (#48)

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,3 +1,9 @@
+/*
+Package handlers - is remote API to call a Register in a FileCoin Network. Contains method exposed by Register to be
+called by Retrieval Gateways and Retrieval Providers in order to get information about current network state.
+
+E.g. - get an updated list of gateways, including just recently enrolled ones
+*/
 package handlers
 
 import (
