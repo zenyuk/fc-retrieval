9a4340370e84b14dc6a81e02a7ed9789d63a7373
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-06-09 09:21:20 +0100

    unit tests for env vars

--- client-js/src/FilecoinRetrievalClient.test.ts
+++ client-js/src/FilecoinRetrievalClient.test.ts
@@ -8,4 +8,27 @@ describe('Client', () => {
     // const offers = await client.findOffersStandardDiscoveryV2()
     expect(['hello']).toEqual(['hello'])
   })
+
+  it('check FCR_REGISTER_API_URL value', async () => {
+    expect(process.env.FCR_REGISTER_API_URL).toEqual('http://register:9020')
+  })
+
+  it('check FCR_LOTUS_AP value', async () => {
+    expect(process.env.FCR_LOTUS_AP).toEqual('http://lotus-full-node:1234/rpc/v0')
+  })
+
+  it('check FCR_LOTUS_AUTH_TOKEN value', async () => {
+    expect(process.env.FCR_LOTUS_AUTH_TOKEN).toBeDefined()
+  })
+
+  it('check FCR_LOTUS_AUTH_TOKEN value', async () => {
+    expect(process.env.FCR_LOTUS_AUTH_TOKEN).toBeDefined()
+  })
+
+  it('check FCR_WALLET_PRIVATE_KEY value', async () => {
+    expect(process.env.FCR_WALLET_PRIVATE_KEY).toBeDefined()
+  })
+  it('check FCR_LOTUS_AUTH_TOKEN value', async () => {
+    expect(process.env.FCR_LOTUS_AUTH_TOKEN).toBeDefined()
+  })
 })
