0aa8c7d92765ba29742fbebea2fafac2e3162c36
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-06-11 10:59:49 +0100

    configurations for unit tests and e2e tests

---
+++ client-js/.env.js
@@ -0,0 +1,4 @@
+process.env.FCR_REGISTER_API_URL="http://register:9020"
+process.env.FCR_LOTUS_AP="http://lotus-full-node:1234/rpc/v0"
+process.env.FCR_LOTUS_AUTH_TOKEN=""
+process.env.FCR_WALLET_PRIVATE_KEY=""
---
+++ client-js/e2e/FilecoinRetrievalClient.test.ts
@@ -0,0 +1,20 @@
+import { FilecoinRetrievalClient } from './index'
+import { defaults } from './constants/defaults'
+
+describe('Client', () => {
+  it('check FCR_REGISTER_API_URL value', async () => {
+    expect(process.env.FCR_REGISTER_API_URL).toEqual('http://register:9020')
+  })
+
+  it('check FCR_LOTUS_AP value', async () => {
+    expect(process.env.FCR_LOTUS_AP).toEqual('http://lotus-full-node:1234/rpc/v0')
+  })
+
+  it('check FCR_LOTUS_AUTH_TOKEN value', async () => {
+    expect(process.env.FCR_LOTUS_AUTH_TOKEN).toBeDefined()
+  })
+
+  it('check FCR_WALLET_PRIVATE_KEY value', async () => {
+    expect(process.env.FCR_WALLET_PRIVATE_KEY).toBeDefined()
+  })
+})
