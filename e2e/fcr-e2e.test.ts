608a2a06f76071d55e94b74106cc3fc8905482a3
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-06-11 12:28:44 +0100

    call register

--- e2e/fcr-e2e.test.ts
+++ e2e/fcr-e2e.test.ts
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 describe('Client', () => {
   it('check FCR_REGISTER_API_URL value', async () => {
     expect(process.env.FCR_REGISTER_API_URL).toEqual('http://register:9020')
@@ -13,5 +15,13 @@ describe('Client', () => {
 
   it('check FCR_WALLET_PRIVATE_KEY value', async () => {
     expect(process.env.FCR_WALLET_PRIVATE_KEY).toBeDefined()
+  })
+
+  it('Call register', async () => {
+    const defaultRegisterURL: string = process.env.FCR_REGISTER_API_URL
+    const url = defaultRegisterURL + '/registers/gateway'
+    const response = await axios.get(url)
+    const gateways = response.data as any[]
+    expect(gateways.length).toBeGreaterThan(0)
   })
 })
