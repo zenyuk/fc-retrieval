3bf41c6addfba96812e5f861dc51a69034bd908c
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-06-14 10:02:09 +0100

    eslint fixes

--- client-js/src/FilecoinRetrievalClient.ts
+++ client-js/src/FilecoinRetrievalClient.ts
@@ -1,55 +1,52 @@
-import { Settings } from './config/settings.config'
-import { FCRPaymentMgr } from './fcrPaymentMgr/payment-manager.class'
-import { GatewaysToUse } from './gateway/gateway.interface'
-import { ContentID } from './cid/cid.interface'
-import { NodeID } from './nodeid/nodeid.interface'
-import { SubCIDOffer } from './cidoffer/subcidoffer.class'
-import { getGatewayByID, getProviderByID } from './register/register.class'
-import { requestStandardDiscoverOffer } from './clientapi/standard_discover_offer_requester'
-import { requestStandardDiscoverV2 } from './clientapi/standard_discover_requester_v2'
-import { GatewayRegister, validateProviderInfo, validateGatewayInfo } from './register/register.class'
-import { requestDHTOfferAck } from './clientapi/dht_offer_ack_requester'
-import { verifyMessage } from './fcrcrypto/msg_signing'
-import {
-  decodeProviderPublishDHTOfferRequest,
-  decodeProviderPublishDHTOfferResponse,
-} from './fcrMessages/provider_publish_dht_offer'
-import { decodeGatewayDHTDiscoverResponseV2, requestDHTDiscoverV2 } from './clientapi/find_offers_dht_discovery_v2'
-import { requestDHTOfferDiscover } from './clientapi/request_dht_offer_discover'
-import BN from 'bn.js'
+import { Settings } from './config/settings.config';
+import { FCRPaymentMgr } from './fcrPaymentMgr/payment-manager.class';
+import { GatewaysToUse } from './gateway/gateway.interface';
+import { ContentID } from './cid/cid.interface';
+import { NodeID } from './nodeid/nodeid.interface';
+import { SubCIDOffer } from './cidoffer/subcidoffer.class';
+import { getGatewayByID, getProviderByID } from './register/register.class';
+import { requestStandardDiscoverOffer } from './clientapi/standard_discover_offer_requester';
+import { requestStandardDiscoverV2 } from './clientapi/standard_discover_requester_v2';
+import { GatewayRegister, validateProviderInfo, validateGatewayInfo } from './register/register.class';
+import { requestDHTOfferAck } from './clientapi/dht_offer_ack_requester';
+import { verifyMessage } from './fcrcrypto/msg_signing';
+import { decodeProviderPublishDHTOfferResponse } from './fcrMessages/provider_publish_dht_offer';
+import { decodeGatewayDHTDiscoverResponseV2, requestDHTDiscoverV2 } from './clientapi/find_offers_dht_discovery_v2';
+import { requestDHTOfferDiscover } from './clientapi/request_dht_offer_discover';
+import BN from 'bn.js';
 
 export interface payResponse {
-  paychAddrs: string
-  voucher: string
-  topup: boolean
-  subCIDOffers: SubCIDOffer[]
+  paychAddrs: string;
+  voucher: string;
+  topup: boolean;
+  subCIDOffers: SubCIDOffer[];
 }
 
 export class FilecoinRetrievalClient {
-  settings: Settings
-  activeGateways: GatewaysToUse
-  gatewaysToUse: GatewaysToUse
-  paymentMgr: FCRPaymentMgr
+  settings: Settings;
+  activeGateways: GatewaysToUse;
+  gatewaysToUse: GatewaysToUse;
+  paymentMgr: FCRPaymentMgr;
 
   constructor(settings: Settings) {
-    this.settings = Object.assign({}, settings)
-    this.activeGateways = {} as GatewaysToUse
-    this.gatewaysToUse = {} as GatewaysToUse
-    this.paymentMgr = {} as FCRPaymentMgr
+    this.settings = Object.assign({}, settings);
+    this.activeGateways = {} as GatewaysToUse;
+    this.gatewaysToUse = {} as GatewaysToUse;
+    this.paymentMgr = {} as FCRPaymentMgr;
   }
 
   // AddActiveGateways adds one or more gateways to active gateway map.
   // Returns the number of gateways added.
   AddGatewaysToUse(): number {
     //
-    return 42
+    return 42;
   }
 
   // AddActiveGateways adds one or more gateways to active gateway map.
   // Returns the number of gateways added.
   AddActiveGateways(gatewayIDs: NodeID[]): number {
     //
-    return 42
+    return 42;
   }
 
   findOffersDHTDiscoveryV2(
@@ -58,26 +55,26 @@ export class FilecoinRetrievalClient {
     numDHT: number,
     offersNumberLimit: number,
   ): Map<string, SubCIDOffer[]> {
-    const offersMap = new Map<string, SubCIDOffer[]>()
+    const offersMap = new Map<string, SubCIDOffer[]>();
 
-    const gw = this.activeGateways[gatewayID.id]
+    const gw = this.activeGateways[gatewayID.id];
 
-    const defaultPaymentLane = new BN(0)
-    const initialRequestPaymentAmount = new BN(numDHT).mul(this.settings.searchPrice)
-    let payResponse = this.paymentMgr.pay(gw.address, defaultPaymentLane, initialRequestPaymentAmount)
+    const defaultPaymentLane = new BN(0);
+    const initialRequestPaymentAmount = new BN(numDHT).mul(this.settings.searchPrice);
+    let payResponse = this.paymentMgr.pay(gw.address, defaultPaymentLane, initialRequestPaymentAmount);
 
     if (payResponse.topup) {
-      this.paymentMgr.topup(gw.address, this.settings.topUpAmount)
+      this.paymentMgr.topup(gw.address, this.settings.topUpAmount);
 
-      payResponse = this.paymentMgr.pay(gw.address, defaultPaymentLane, initialRequestPaymentAmount)
+      payResponse = this.paymentMgr.pay(gw.address, defaultPaymentLane, initialRequestPaymentAmount);
       if (payResponse.topup) {
         // Unable to make payment for initial DHT offers discovery
-        return offersMap
+        return offersMap;
       }
     }
 
-    const nonce = 0
-    const ttl = 0
+    const nonce = 0;
+    const ttl = 0;
     const request = requestDHTDiscoverV2(
       gw,
       contentID,
@@ -87,63 +84,63 @@ export class FilecoinRetrievalClient {
       false,
       payResponse.paychAddrs,
       payResponse.voucher,
-    )
-    let addedSubOffersCount = 0
-    let offersDigestsFromAllGateways: string[][] = [[]]
+    );
+    let addedSubOffersCount = 0;
+    let offersDigestsFromAllGateways: string[][] = [[]];
 
     for (let i = 0; i < request.contactedResp.length; i++) {
-      const contactedGatewayID = request.contactedGateways[i]
-      const resp = request.contactedResp[i]
-      const gatewayInfo = getGatewayByID(this.settings.registerURL, contactedGatewayID)
+      const contactedGatewayID = request.contactedGateways[i];
+      const resp = request.contactedResp[i];
+      const gatewayInfo = getGatewayByID(this.settings.registerURL, contactedGatewayID);
       if (!this.validateGatewayInfo(gatewayInfo)) {
         // logging.Error("Gateway register info not valid.")
-        continue
+        continue;
       }
-      const pubKey = gatewayInfo.getSigningKey()
+      const pubKey = gatewayInfo.getSigningKey();
       if (pubKey == undefined) {
         //logging.Error('Fail to obtain public key.')
-        continue
+        continue;
       }
       if (!resp.verify(pubKey)) {
         //logging.Error('Fail to verify sub response.')
-        continue
+        continue;
       }
 
-      const decoded = decodeGatewayDHTDiscoverResponseV2(resp)
+      const decoded = decodeGatewayDHTDiscoverResponseV2(resp);
       if (decoded === undefined) {
         // logging.Error('Fail to decode response')
-        continue
+        continue;
       }
       if (!decoded.found) {
-        return offersMap
+        return offersMap;
       }
       // comply with given offers number limit
       if (addedSubOffersCount + decoded.subCidOffersDigest.length > offersNumberLimit) {
-        offersDigestsFromAllGateways.push(decoded.subCidOffersDigest.slice(0, offersNumberLimit - addedSubOffersCount))
+        offersDigestsFromAllGateways.push(decoded.subCidOffersDigest.slice(0, offersNumberLimit - addedSubOffersCount));
       } else {
-        offersDigestsFromAllGateways.push(decoded.subCidOffersDigest)
+        offersDigestsFromAllGateways.push(decoded.subCidOffersDigest);
       }
-      addedSubOffersCount += decoded.subCidOffersDigest.length
+      addedSubOffersCount += decoded.subCidOffersDigest.length;
       if (addedSubOffersCount >= offersNumberLimit) {
-        break
+        break;
       }
     }
-
-    let unit = 0
+    /*
+    let unit = 0;
     for (let entry in offersDigestsFromAllGateways) {
-      unit += entry.length
+      unit += entry.length;
     }
-    const offerRequestPaymentAmount = this.settings.offerPrice.mul(new BN(unit))
-
-    payResponse = this.paymentMgr.pay(gw.address, defaultPaymentLane, initialRequestPaymentAmount)
+     const offerRequestPaymentAmount = this.settings.offerPrice.mul(new BN(unit))
+    */
+    payResponse = this.paymentMgr.pay(gw.address, defaultPaymentLane, initialRequestPaymentAmount);
 
     if (payResponse.topup) {
-      this.paymentMgr.topup(gw.address, this.settings.topUpAmount)
+      this.paymentMgr.topup(gw.address, this.settings.topUpAmount);
 
-      payResponse = this.paymentMgr.pay(gw.address, defaultPaymentLane, initialRequestPaymentAmount)
+      payResponse = this.paymentMgr.pay(gw.address, defaultPaymentLane, initialRequestPaymentAmount);
       if (payResponse.topup) {
         // Unable to make payment for initial DHT offers discovery
-        return offersMap
+        return offersMap;
       }
     }
 
@@ -155,73 +152,73 @@ export class FilecoinRetrievalClient {
       offersDigestsFromAllGateways,
       payResponse.paychAddrs,
       payResponse.voucher,
-    )
+    );
 
     for (let entry of gatewaySubOffers) {
-      offersMap.set(entry.gatewayID.id, entry.subOffers)
+      offersMap.set(entry.gatewayID.id, entry.subOffers);
     }
 
-    return offersMap
+    return offersMap;
   }
 
   validateGatewayInfo = (gatewayInfo: GatewayRegister): boolean => {
-    return false
-  }
+    return false;
+  };
 
   async FindDHTOfferAck(contentID: ContentID, gatewayID: NodeID, providerID: NodeID): Promise<boolean> {
-    const provider = getProviderByID(this.settings.registerURL, providerID.id)
+    const provider = getProviderByID(this.settings.registerURL, providerID.id);
 
-    const pvalidation = validateProviderInfo(provider)
+    const pvalidation = validateProviderInfo(provider);
     if (!pvalidation) {
-      throw new Error('Invalid register info')
+      throw new Error('Invalid register info');
     }
 
-    const dhtOfferAckResponse = await requestDHTOfferAck(provider, contentID, gatewayID)
+    const dhtOfferAckResponse = await requestDHTOfferAck(provider, contentID, gatewayID);
     if (!dhtOfferAckResponse.found) {
-      return false
+      return false;
     }
 
-    const gateway = getGatewayByID(this.settings.registerURL, gatewayID)
+    const gateway = getGatewayByID(this.settings.registerURL, gatewayID);
 
-    const gvalidation = validateGatewayInfo(gateway)
+    const gvalidation = validateGatewayInfo(gateway);
     if (!gvalidation) {
-      throw new Error('Invalid register info')
+      throw new Error('Invalid register info');
     }
 
-    const gwPubKey = gateway.getSigningKey()
-    const pvdPubKey = provider.getSigningKey()
+    const gwPubKey = gateway.getSigningKey();
+    const pvdPubKey = provider.getSigningKey();
 
-    dhtOfferAckResponse.offerRequest.verify(pvdPubKey)
+    dhtOfferAckResponse.offerRequest.verify(pvdPubKey);
 
-    const offers = decodeProviderPublishDHTOfferRequest(dhtOfferAckResponse.offerRequest)
-    const found = false
-    for (const offer in offers) {
-      // ?
-    }
+    // const offers = decodeProviderPublishDHTOfferRequest(dhtOfferAckResponse.offerRequest);
+    const found = false;
+    // for (const offer in offers) {
+    //   // ?
+    // }
     if (!found) {
-      throw new Error('Initial request does not contain the given cid')
+      throw new Error('Initial request does not contain the given cid');
     }
 
-    const verified = dhtOfferAckResponse.offerResponse.verify(pvdPubKey)
+    const verified = dhtOfferAckResponse.offerResponse.verify(pvdPubKey);
     if (!verified) {
-      throw new Error('Error in verifying the ack')
+      throw new Error('Error in verifying the ack');
     }
 
-    const dhtOfferResponse = decodeProviderPublishDHTOfferResponse(dhtOfferAckResponse.offerResponse)
-    verifyMessage(gwPubKey, dhtOfferResponse.signature, dhtOfferAckResponse.offerRequest)
+    const dhtOfferResponse = decodeProviderPublishDHTOfferResponse(dhtOfferAckResponse.offerResponse);
+    verifyMessage(gwPubKey, dhtOfferResponse.signature, dhtOfferAckResponse.offerRequest);
 
-    return true
+    return true;
   }
 
   // FindOffersStandardDiscoveryV2 finds offer using standard discovery from given gateways
   async findOffersStandardDiscoveryV2(cid: ContentID, gatewayID: NodeID, maxOffers: number) {
-    const gw = this.activeGateways[gatewayID.id]
+    const gw = this.activeGateways[gatewayID.id];
 
-    let payResponse = this.paymentMgr.pay(gw.address, new BN(0), this.settings.searchPrice)
+    let payResponse = this.paymentMgr.pay(gw.address, new BN(0), this.settings.searchPrice);
 
     if (payResponse.topup == true) {
-      this.paymentMgr.topup(gw.nodeID, this.settings.topUpAmount)
-      payResponse = this.paymentMgr.pay(gw.address, new BN(0), this.settings.searchPrice)
+      this.paymentMgr.topup(gw.nodeID, this.settings.topUpAmount);
+      payResponse = this.paymentMgr.pay(gw.address, new BN(0), this.settings.searchPrice);
     }
 
     const offerDigests = await requestStandardDiscoverV2(
@@ -231,7 +228,7 @@ export class FilecoinRetrievalClient {
       Date.now() + this.settings.establishmentTTL,
       payResponse.paychAddrs,
       payResponse.voucher,
-    )
+    );
 
     const offers = await requestStandardDiscoverOffer(
       gw,
@@ -241,26 +238,26 @@ export class FilecoinRetrievalClient {
       offerDigests,
       payResponse.paychAddrs,
       payResponse.voucher,
-    )
+    );
 
-    const validOffers = [] as SubCIDOffer[]
+    const validOffers = [] as SubCIDOffer[];
     for (const offer of offers) {
-      const providerInfo = getProviderByID(this.settings.registerURL, offer.getProviderID())
-      const pubKey = providerInfo.signingKey
+      const providerInfo = getProviderByID(this.settings.registerURL, offer.getProviderID());
+      const pubKey = providerInfo.signingKey;
       if (offer.verify(pubKey) != null) {
         // console.log('Offer signature fail to verify.')
-        continue
+        continue;
       }
       if (offer.verifyMerkleProof() != null) {
         // console.log('Merkle proof verification failed.')
-        continue
+        continue;
       }
 
-      validOffers.push(offer)
+      validOffers.push(offer);
       if (validOffers.length >= maxOffers) {
-        break
+        break;
       }
     }
-    return validOffers
+    return validOffers;
   }
 }
--- client-js/src/clientapi/dht_offer_ack_requester.ts
+++ client-js/src/clientapi/dht_offer_ack_requester.ts
@@ -1,28 +1,27 @@
-import { ContentID } from '../cid/cid.interface'
-import { FCRMessage } from '../fcrMessages/fcrMessage.class'
-import { NodeID } from '../nodeid/nodeid.interface'
-import { ProviderRegister } from '../register/register.class'
+import { ContentID } from '../cid/cid.interface';
+import { NodeID } from '../nodeid/nodeid.interface';
+import { ProviderRegister } from '../register/register.class';
 import {
   encodeClientDHTOfferAckRequest,
   decodeClientDHTOfferAckResponse,
   DHTOfferAckResponse,
-} from '../fcrMessages/client_dht_offer_ack'
-import { sendMessage } from '../request/request'
+} from '../fcrMessages/client_dht_offer_ack';
+import { sendMessage } from '../request/request';
 
 export const requestDHTOfferAck = async (
   providerInfo: ProviderRegister,
   cid: ContentID,
   gatewayID: NodeID,
 ): Promise<DHTOfferAckResponse> => {
-  const request = encodeClientDHTOfferAckRequest(cid, gatewayID)
+  const request = encodeClientDHTOfferAckRequest(cid, gatewayID);
 
-  const response = await sendMessage(providerInfo.networkInfoClient, request)
+  const response = await sendMessage(providerInfo.networkInfoClient, request);
 
-  const pubKey = providerInfo.getSigningKey()
+  const pubKey = providerInfo.getSigningKey();
 
-  response.verify(pubKey)
+  response.verify(pubKey);
 
-  const dhtOfferAckResponse = decodeClientDHTOfferAckResponse(response)
+  const dhtOfferAckResponse = decodeClientDHTOfferAckResponse(response);
 
-  return dhtOfferAckResponse
-}
+  return dhtOfferAckResponse;
+};
