eb0153f896c1541f5b66633755da050f278188d9
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-06-14 10:02:09 +0100

    eslint fixes

--- src/fcrMessages/provider_publish_dht_offer.ts
+++ src/fcrMessages/provider_publish_dht_offer.ts
@@ -1,7 +1,6 @@
-import { ContentID } from '../cid/cid.interface'
-import { NodeID } from '../nodeid/nodeid.interface'
-import { CIDOffer } from '../cidoffer/cidoffer.class'
-import { FCRMessage } from './fcrMessage.class'
+import { NodeID } from '../nodeid/nodeid.interface';
+import { CIDOffer } from '../cidoffer/cidoffer.class';
+import { FCRMessage } from './fcrMessage.class';
 
 // // providerPublishDHTOfferRequest is the request from provider to gateway to publish dht offer
 // type providerPublishDHTOfferRequest struct {
@@ -12,14 +11,14 @@ import { FCRMessage } from './fcrMessage.class'
 // }
 
 export interface ProviderPublishDHTOfferRequest {
-  providerID: NodeID // provider id
-  nonce: number // nonce
-  offers: CIDOffer[] // offers
+  providerID: NodeID; // provider id
+  nonce: number; // nonce
+  offers: CIDOffer[]; // offers
 }
 
 export interface ProviderPublishDHTOfferResponse {
-  nonce: number
-  signature: string
+  nonce: number;
+  signature: string;
 }
 
 // EncodeProviderPublishDHTOfferRequest is used to get the FCRMessage of providerPublishDHTOfferRequest
@@ -28,20 +27,20 @@ export const encodeProviderPublishDHTOfferRequest = (
   nonce: number,
   offers: CIDOffer[],
 ): FCRMessage => {
-  return {} as FCRMessage
-}
+  return {} as FCRMessage;
+};
 
 // DecodeProviderPublishDHTOfferRequest is used to get the fields from FCRMessage of providerPublishDHTOfferRequest
 export const decodeProviderPublishDHTOfferRequest = (fcrMsg: FCRMessage): ProviderPublishDHTOfferRequest => {
-  return {} as ProviderPublishDHTOfferRequest
-}
+  return {} as ProviderPublishDHTOfferRequest;
+};
 
 // EncodeProviderPublishDHTOfferRequest is used to get the FCRMessage of providerPublishDHTOfferRequest
 export const encodeProviderPublishDHTOfferResponse = (nonce: number, signature: string): FCRMessage => {
-  return {} as FCRMessage
-}
+  return {} as FCRMessage;
+};
 
 // DecodeProviderPublishDHTOfferRequest is used to get the fields from FCRMessage of providerPublishDHTOfferRequest
 export const decodeProviderPublishDHTOfferResponse = (fcrMsg: FCRMessage): ProviderPublishDHTOfferResponse => {
-  return {} as ProviderPublishDHTOfferResponse
-}
+  return {} as ProviderPublishDHTOfferResponse;
+};
