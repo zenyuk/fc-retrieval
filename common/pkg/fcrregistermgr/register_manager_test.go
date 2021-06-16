0e31f424ea1f1747545c20c4db05d4cf7fe13c5e
zenyuk <33681156+zenyuk@users.noreply.github.com>
2021-06-16 13:33:24 +1000

    181 updte register manager (#58)

--- common/pkg/fcrregistermgr/register_manager_test.go
+++ common/pkg/fcrregistermgr/register_manager_test.go
@@ -1,7 +1,6 @@
 package fcrregistermgr
 
 import (
-  "fmt"
   "reflect"
   "sync"
   "testing"
@@ -23,17 +22,72 @@ func TestFCRRegisterMgr_GetGateway(t *testing.T) {
 	nodeID5A, _ := nodeid.NewNodeIDFromHexString("5A")
 	nodeIDFFFF, _ := nodeid.NewNodeIDFromHexString("FFFF")
 
-	gr := map[string]register.GatewayRegister{}
-	gr[nodeID00.ToString()] = register.GatewayRegister{NodeID: nodeID00.ToString()}
-	gr[nodeID01.ToString()] = register.GatewayRegister{NodeID: nodeID01.ToString()}
-	gr[nodeID02.ToString()] = register.GatewayRegister{NodeID: nodeID02.ToString()}
-	gr[nodeID5A.ToString()] = register.GatewayRegister{NodeID: nodeID5A.ToString()}
-	gr[nodeIDFFFF.ToString()] = register.GatewayRegister{NodeID: nodeIDFFFF.ToString()}
+	gr := map[string]register.GatewayRegistrar{}
+  gr[nodeID00.ToString()] = register.NewGatewayRegister(
+    nodeID00.ToString(),
+    "address",
+    "rootSigningKey",
+    "signingKey",
+    "regionCode",
+    "networkInfoGateway",
+    "networkInfoProvider",
+    "networkInfoClient",
+    "networkInfoAdmin",
+    nil,
+  )
+  gr[nodeID01.ToString()] = register.NewGatewayRegister(
+    nodeID01.ToString(),
+    "address",
+    "rootSigningKey",
+    "signingKey",
+    "regionCode",
+    "networkInfoGateway",
+    "networkInfoProvider",
+    "networkInfoClient",
+    "networkInfoAdmin",
+    nil,
+  )
+  gr[nodeID02.ToString()] = register.NewGatewayRegister(
+    nodeID02.ToString(),
+    "address",
+    "rootSigningKey",
+    "signingKey",
+    "regionCode",
+    "networkInfoGateway",
+    "networkInfoProvider",
+    "networkInfoClient",
+    "networkInfoAdmin",
+    nil,
+  )
+  gr[nodeID5A.ToString()] = register.NewGatewayRegister(
+    nodeID5A.ToString(),
+    "address",
+    "rootSigningKey",
+    "signingKey",
+    "regionCode",
+    "networkInfoGateway",
+    "networkInfoProvider",
+    "networkInfoClient",
+    "networkInfoAdmin",
+    nil,
+  )
+  gr[nodeIDFFFF.ToString()] = register.NewGatewayRegister(
+    nodeIDFFFF.ToString(),
+    "address",
+    "rootSigningKey",
+    "signingKey",
+    "regionCode",
+    "networkInfoGateway",
+    "networkInfoProvider",
+    "networkInfoClient",
+    "networkInfoAdmin",
+    nil,
+  )
 
 	type fields struct {
 		start                     bool
 		gatewayDiscv              bool
-		registeredGatewaysMap     map[string]register.GatewayRegister
+		registeredGatewaysMap     map[string]register.GatewayRegistrar
 		registeredGatewaysMapLock sync.RWMutex
 	}
 	type args struct {
@@ -43,7 +97,7 @@ func TestFCRRegisterMgr_GetGateway(t *testing.T) {
 		name   string
 		fields fields
 		args   args
-		want   *register.GatewayRegister
+		want   register.GatewayRegistrar
 	}{
 		{name: "getGateway not started returns nil",
 			fields: fields{
@@ -73,7 +127,18 @@ func TestFCRRegisterMgr_GetGateway(t *testing.T) {
 				registeredGatewaysMapLock: sync.RWMutex{},
 			},
 			args: struct{ id *nodeid.NodeID }{id: nodeID00},
-			want: &register.GatewayRegister{NodeID: nodeID00.ToString()},
+			want: register.NewGatewayRegister(
+        nodeID00.ToString(),
+        "address",
+        "rootSigningKey",
+        "signingKey",
+        "regionCode",
+        "networkInfoGateway",
+        "networkInfoProvider",
+        "networkInfoClient",
+        "networkInfoAdmin",
+        nil,
+      ),
 		},
 	}
 
@@ -99,24 +164,79 @@ func TestFCRRegisterMgr_GetAllGateways(t *testing.T) {
 	nodeID5A, _ := nodeid.NewNodeIDFromHexString("5A")
 	nodeIDFFFF, _ := nodeid.NewNodeIDFromHexString("FFFF")
 
-	gr := map[string]register.GatewayRegister{}
-	gr[nodeID00.ToString()] = register.GatewayRegister{NodeID: nodeID00.ToString()}
-	gr[nodeID01.ToString()] = register.GatewayRegister{NodeID: nodeID01.ToString()}
-	gr[nodeID02.ToString()] = register.GatewayRegister{NodeID: nodeID02.ToString()}
-	gr[nodeID5A.ToString()] = register.GatewayRegister{NodeID: nodeID5A.ToString()}
-	gr[nodeIDFFFF.ToString()] = register.GatewayRegister{NodeID: nodeIDFFFF.ToString()}
+	gr := map[string]register.GatewayRegistrar{}
+	gr[nodeID00.ToString()] = register.NewGatewayRegister(
+    nodeID00.ToString(),
+    "address",
+    "rootSigningKey",
+    "signingKey",
+    "regionCode",
+    "networkInfoGateway",
+    "networkInfoProvider",
+    "networkInfoClient",
+    "networkInfoAdmin",
+    nil,
+  )
+  gr[nodeID01.ToString()] = register.NewGatewayRegister(
+    nodeID01.ToString(),
+    "address",
+    "rootSigningKey",
+    "signingKey",
+    "regionCode",
+    "networkInfoGateway",
+    "networkInfoProvider",
+    "networkInfoClient",
+    "networkInfoAdmin",
+    nil,
+  )
+  gr[nodeID02.ToString()] = register.NewGatewayRegister(
+    nodeID02.ToString(),
+    "address",
+    "rootSigningKey",
+    "signingKey",
+    "regionCode",
+    "networkInfoGateway",
+    "networkInfoProvider",
+    "networkInfoClient",
+    "networkInfoAdmin",
+    nil,
+  )
+  gr[nodeID5A.ToString()] = register.NewGatewayRegister(
+    nodeID5A.ToString(),
+    "address",
+    "rootSigningKey",
+    "signingKey",
+    "regionCode",
+    "networkInfoGateway",
+    "networkInfoProvider",
+    "networkInfoClient",
+    "networkInfoAdmin",
+    nil,
+  )
+  gr[nodeIDFFFF.ToString()] = register.NewGatewayRegister(
+    nodeIDFFFF.ToString(),
+    "address",
+    "rootSigningKey",
+    "signingKey",
+    "regionCode",
+    "networkInfoGateway",
+    "networkInfoProvider",
+    "networkInfoClient",
+    "networkInfoAdmin",
+    nil,
+  )
 
 	type fields struct {
 		start                     bool
 		gatewayDiscv              bool
-		registeredGatewaysMap     map[string]register.GatewayRegister
+		registeredGatewaysMap     map[string]register.GatewayRegistrar
 		registeredGatewaysMapLock sync.RWMutex
 	}
 	tests := []struct {
 		name   string
 		fields fields
 
-		want []register.GatewayRegister
+		want []register.GatewayRegistrar
 	}{
 		{name: "getGateway not started returns nil",
 			fields: fields{
@@ -146,13 +266,67 @@ func TestFCRRegisterMgr_GetAllGateways(t *testing.T) {
 				registeredGatewaysMapLock: sync.RWMutex{},
 			},
 
-			want: []register.GatewayRegister{
-
-				{NodeID: nodeID02.ToString()},
-				{NodeID: nodeID5A.ToString()},
-				{NodeID: nodeIDFFFF.ToString()},
-				{NodeID: nodeID00.ToString()},
-				{NodeID: nodeID01.ToString()},
+			want: []register.GatewayRegistrar{
+        register.NewGatewayRegister(
+          nodeID02.ToString(),
+          "address",
+          "rootSigningKey",
+          "signingKey",
+          "regionCode",
+          "networkInfoGateway",
+          "networkInfoProvider",
+          "networkInfoClient",
+          "networkInfoAdmin",
+          nil,
+        ),
+        register.NewGatewayRegister(
+          nodeID5A.ToString(),
+          "address",
+          "rootSigningKey",
+          "signingKey",
+          "regionCode",
+          "networkInfoGateway",
+          "networkInfoProvider",
+          "networkInfoClient",
+          "networkInfoAdmin",
+          nil,
+        ),
+        register.NewGatewayRegister(
+          nodeIDFFFF.ToString(),
+          "address",
+          "rootSigningKey",
+          "signingKey",
+          "regionCode",
+          "networkInfoGateway",
+          "networkInfoProvider",
+          "networkInfoClient",
+          "networkInfoAdmin",
+          nil,
+        ),
+        register.NewGatewayRegister(
+          nodeID00.ToString(),
+          "address",
+          "rootSigningKey",
+          "signingKey",
+          "regionCode",
+          "networkInfoGateway",
+          "networkInfoProvider",
+          "networkInfoClient",
+          "networkInfoAdmin",
+          nil,
+        ),
+        register.NewGatewayRegister(
+          nodeID01.ToString(),
+          "address",
+          "rootSigningKey",
+          "signingKey",
+          "regionCode",
+          "networkInfoGateway",
+          "networkInfoProvider",
+          "networkInfoClient",
+          "networkInfoAdmin",
+          nil,
+        ),
 			},
 		},
 	}
@@ -169,50 +343,4 @@ func TestFCRRegisterMgr_GetAllGateways(t *testing.T) {
 			assert.ElementsMatch(t, got, tt.want)
 		})
 	}
-}
-
-func ExampleFCRRegisterMgr_GetRegisteredGateways() {
-  v, err := rm.GetRegisteredGateways()
-  fmt.Println(v, err)
-  // Output:
-  // [] Get "fakeRegisterAPIURL/registers/gateway": unsupported protocol scheme ""
-}
-
-func ExampleFCRRegisterMgr_GetGatewayByID_nil() {
-  v, err := rm.GetGatewayByID(nil)
-  fmt.Println(v, err)
-  // Output:
-  // {         <nil>} gateway ID is not provided in GetGatewayByID
-}
-
-func ExampleFCRRegisterMgr_GetGatewayByID_new() {
-  nd := &nodeid.NodeID{}
-  v, err := rm.GetGatewayByID(nd)
-  fmt.Println(v, err)
-  // Output:
-  // {         <nil>} Get "fakeRegisterAPIURL/registers/gateway/00": unsupported protocol scheme ""
-}
-
-func ExampleFCRRegisterMgr_GetRegisteredProviders() {
-  v, err := rm.GetRegisteredProviders()
-  fmt.Println(v, err)
-  // Output:
-  // [] Get "fakeRegisterAPIURL/registers/provider": unsupported protocol scheme ""
-}
-
-func ExampleFCRRegisterMgr_GetProviderByID_nil() {
-  v, err := rm.GetProviderByID(nil)
-  fmt.Println(v, err)
-  // Output:
-  // {        <nil>} provider ID is not provided in GetProviderByID
-
-}
-
-func ExampleFCRRegisterMgr_GetProviderByID_new() {
-  nd := &nodeid.NodeID{}
-  v, err := rm.GetProviderByID(nd)
-  fmt.Println(v, err)
-  // Output:
-  // {        <nil>} Get "fakeRegisterAPIURL/registers/provider/00": unsupported protocol scheme ""
-
 }
