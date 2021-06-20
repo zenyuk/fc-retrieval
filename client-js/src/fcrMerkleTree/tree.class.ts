b57089f7b7f406b5223b97bb4d0822fab5379b13
Raphael Pralat <raphael.pralat@consensys.net>
2021-06-20 21:33:37 +0200

    feat: merkle verification

--- client-js/src/fcrMerkleTree/tree.class.ts
+++ client-js/src/fcrMerkleTree/tree.class.ts
@@ -1,20 +1,37 @@
-import { Content, MerkleTree } from './types'
-import { FCRMerkleProof } from './proof.class'
+import { Content } from './types'
+const { MerkleTree } = require('merkletreejs')
+const SHA256 = require('crypto-js/sha256')
+import { MerkleProof } from './proof'
 
 export class FCRMerkleTree {
-  tree: MerkleTree
+  tree: any
 
-  constructor(contents: Content[]) {
-    // TODO
+  /**
+   * Create merkle tree
+   * @param contents string[]
+   */
+  constructor(contents: string[]) {
+    if (contents.length % 2 != 0) {
+      contents.push(contents[contents.length - 1])
+    }
+    const hashedContents = contents.map(x => SHA256(x))
+    this.tree = new MerkleTree(hashedContents, SHA256)
   }
 
+  /**
+   * Get merkle root
+   * @returns string
+   */
   getMerkleRoot(): string {
-    // TODO
-    return ''
+    return this.tree.getRoot().toString('hex')
   }
 
-  generateMerkleProof(content: Content): FCRMerkleProof {
-    // TODO
-    return {} as FCRMerkleProof
+  /**
+   * Generate merkle proof
+   * @param content
+   * @returns string
+   */
+  generateMerkleProof(content: Content): MerkleProof[] {
+    return this.tree.getProof(SHA256(content))
   }
 }
