3a67d31569fa14a9cd8eb1df75520253366e71d2
Raphael Pralat <raphael.pralat@consensys.net>
2021-06-21 15:52:57 +0200

    feat: shaa256

--- src/fcrMerkleTree/proof.ts
+++ src/fcrMerkleTree/proof.ts
@@ -1,4 +1,5 @@
 const { MerkleTree } = require('merkletreejs')
+const SHA256 = require('crypto-js/sha256')
 
 export interface MerkleProof {
   position: string
@@ -9,6 +10,6 @@ export interface MerkleProof {
 }
 
 export const verifyContent = (merkleProof: MerkleProof[], leaf: string, root: string): boolean => {
-  const tree = new MerkleTree([])
+  const tree = new MerkleTree([], SHA256)
   return tree.verify(merkleProof, leaf, root)
 }
