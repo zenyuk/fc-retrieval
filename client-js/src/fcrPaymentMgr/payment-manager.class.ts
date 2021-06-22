6353f3a413798221573aaf9139f74ea73c6c547a
wcgcyx <wcgcyx@gmail.com>
2021-06-22 13:55:23 +1000

    Add paymentmgr implementation

--- client-js/src/fcrPaymentMgr/payment-manager.class.ts
+++ client-js/src/fcrPaymentMgr/payment-manager.class.ts
@@ -1,43 +1,115 @@
-import { Address } from './types'
+import { FilecoinRPC } from './types'
 import { payResponse } from '../FilecoinRetrievalClient'
-import BN from 'bn.js'
+import { BigNumber } from "bignumber.js";
 
-export interface LaneState {
+const filecoin_signer = require('@zondax/filecoin-signing-tools')
+
+export class LaneState {
   nonce: number
-  redeemed: string
+  redeemed: BigNumber
   vouchers: string[]
+
+  constructor() {
+    this.nonce = 0
+    this.redeemed = new BigNumber(0)
+    this.vouchers = new Array()
+  }
 }
 
-export interface ChannelState {
-  addr: Address
-  balance: string
-  redeemed: string
+export class ChannelState {
+  addr: string
+  balance: BigNumber
+  redeemed: BigNumber
   laneStates: Map<number, LaneState>
+
+  constructor(addr: string, balance: BigNumber) {
+    this.addr = addr
+    this.balance = balance
+    this.redeemed = new BigNumber(0)
+    this.laneStates = {} as Map<number, LaneState>
+  }
 }
 
 export class FCRPaymentMgr {
-  privKey: string
-  address: Address
-  authToken: string
-  lotusAPIAddr: string
+  recoveredKey: any
+  filRPC: any
+  header: any
+
   outboundChs: Map<string, ChannelState>
-  inboundChs: Map<string, ChannelState>
 
   constructor(privateKey: string, lotusAPIAddr: string, authToken: string) {
-    // TODO
-    this.privKey = ''
-    this.address = {} as Address
-    this.authToken = ''
-    this.lotusAPIAddr = ''
+    this.recoveredKey = filecoin_signer.keyRecover(privateKey)
+    this.filRPC = new FilecoinRPC({url: lotusAPIAddr, token: authToken})
+    this.header = { 'Authorization': `Bearer ${authToken}` }
+    
     this.outboundChs = {} as Map<string, ChannelState>
-    this.inboundChs = {} as Map<string, ChannelState>
   }
 
-  topup(recipient: string, amount: BN) {
-    // TODO
+  topup(recipient: string, amount: BigNumber) {
+    if (recipient in this.outboundChs) {
+      // There is an existing channel, TODO. Topup
+      var nonce = this.filRPC.getNonce(this.recoveredKey.address)
+      nonce = nonce.result
+      var topup = {
+        from: this.recoveredKey.address,
+        to: recipient,
+        nonce: nonce,
+        value: amount.toString(10),
+        GasLimit: '0',
+        GasFeeCap: '0',
+        GasPremium: '0',
+        Method: 0,
+        Params: ""
+      } as any
+      topup = this.filRPC.getGasEstimation(topup)
+      if ('result' in topup) {
+        topup = topup.result
+      } else {
+        console.log("Error in estimating gas cost")
+        return 1
+      }
+      var signedMessage = JSON.parse(filecoin_signer.transactionSignLotus(topup, this.recoveredKey.privateKey))// Send message
+      var res = this.filRPC.sendSignedMessage(signedMessage)
+      // TODO
+      console.log(res)
+      var cs = this.outboundChs.get(recipient)
+      if (cs != undefined) {
+        cs.balance = cs.balance.plus(amount)
+      } else {
+        console.log("Internal error")
+        return 1
+      }
+      return 0
+    } else {
+      // Need to create a channel
+      // Get nonce
+      var nonce = this.filRPC.getNonce(this.recoveredKey.address)
+      nonce = nonce.result
+      let create_channel = filecoin_signer.createPymtChan(this.recoveredKey.address, recipient, amount.toString(), nonce, '0', '0', '0')
+      create_channel = this.filRPC.getGasEstimation(create_channel)
+      if ('result' in create_channel) {
+        create_channel = create_channel.result
+      } else {
+        console.log("Error in estimating gas cost")
+        return 1
+      }
+      var signedMessage = JSON.parse(filecoin_signer.transactionSignLotus(create_channel, this.recoveredKey.privateKey))
+      // Send message
+      var res = this.filRPC.sendSignedMessage(signedMessage)
+      if (res.result.ReturnDec.IDAddress == undefined) {
+        // Error in creating payment channel
+        console.log("Error in creating payment channel")
+        return 1
+      }
+      // Success, add a new entry to the out bound channels
+      var cAddr = String(res.result.ReturnDec.IDAddress)
+      this.outboundChs.set(cAddr, new ChannelState(cAddr, amount))
+      console.log(cAddr)
+    }
+    return 0
   }
 
-  pay(address: string, defaultPaymentLane: BN, initialRequestPaymentAmount: BN) {
+  pay(recipient: string, lane: number, amount: BigNumber) {
     return { paychAddrs: '', voucher: '', topup: false } as payResponse
   }
 }
--- client-js/src/fcrPaymentMgr/types.ts
+++ client-js/src/fcrPaymentMgr/types.ts
@@ -1,1 +1,90 @@
+import axios from 'axios';
+import { BigNumber } from "bignumber.js";
+
 export type Address = any;
+
+class FilecoinRPC {
+    requester: any
+    constructor(args: { url: any; token: any }) {
+      if (!('url' in args && 'token' in args)) {
+        throw new Error('FilecoinRPC required an `url` and a `token` to communicate with the node.')
+      }
+  
+      this.requester = axios.create({
+        baseURL: args.url,
+        headers: { Authorization: `Bearer ${args.token}` },
+      })
+    }
+  
+    async getNonce(address: any) {
+      let response = await this.requester.post('', {
+        jsonrpc: '2.0',
+        method: 'Filecoin.MpoolGetNonce',
+        id: 1,
+        params: [address],
+      })
+  
+      return response.data
+    }
+  
+    async sendSignedMessage(signedMessage: any) {
+      let response = await this.requester.post('', {
+        jsonrpc: '2.0',
+        method: 'Filecoin.MpoolPush',
+        id: 1,
+        params: [signedMessage],
+      })
+  
+      if ('error' in response.data) {
+        throw new Error(response.data.error.message)
+      }
+  
+      let cid = response.data.result
+  
+      response = await this.requester.post('', {
+        jsonrpc: '2.0',
+        method: 'Filecoin.StateWaitMsg',
+        id: 1,
+        params: [cid, null],
+      })
+  
+      return response.data
+    }
+  
+    async getGasEstimation(message: any) {
+      let response = await this.requester.post('', {
+        jsonrpc: '2.0',
+        method: 'Filecoin.GasEstimateMessageGas',
+        id: 1,
+        params: [message, { MaxFee: '0' }, null],
+      })
+  
+      return response.data
+    }
+  
+    async readState(address: any) {
+      let response = await this.requester.post('', {
+        jsonrpc: '2.0',
+        method: 'Filecoin.StateReadState',
+        id: 1,
+        params: [address, null],
+      })
+  
+      return response.data
+    }
+  }
+
+  export declare class Message {
+    Version?: number;
+    To: string;
+    From: string;
+    Nonce: number;
+    Value: BigNumber;
+    GasLimit: number;
+    GasFeeCap: BigNumber;
+    GasPremium: BigNumber;
+    Method: number;
+    Params: string;
+}
+
+export {FilecoinRPC}
