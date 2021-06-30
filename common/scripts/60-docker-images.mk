99dd4c6aba3ca623d88cf1fd46709bf8c57e0fcc
James Fan <james.fan@consensys.net>
2021-07-01 08:41:46 +1000

    add 'make lotus-images' for consistency

--- common/scripts/60-docker-images.mk
+++ common/scripts/60-docker-images.mk
@@ -1,4 +1,13 @@
 
+
+lotus-images:
+	@echo " \\e[01;32m \\n#run: $@\\e[m"
+	set -e; \
+	docker image ls | egrep '^REPOSITORY|consensys'; \
+	echo; \
+	cd itest; \
+	make lotusbase lotusdaemon lotusfullnode
+
 # make micro docker images
 micro-images: register/main provider/main gateway/main
 	@echo " \\e[01;32m \\n#run: $@\\e[m"
--- common/scripts/70-circle-ci.mk
+++ common/scripts/70-circle-ci.mk
@@ -4,20 +4,16 @@
 
 ci-1 : deps build-servers micro-images coverage
 
-ci-2 ci-lotus:
-	set -e; \
-	docker image ls | egrep '^REPOSITORY|consensys'; \
-	echo; \
-	cd itest; \
-	make lotusbase lotusdaemon lotusfullnode
+ci-2 : lotus-images
 
 ci-3 : build-servers-test
 
-ci-4 ci-poc1 :
-	$(MAKE) LAST_GATEWAY_NO=0 itest-poc1 clean
+ci-4 : itest-poc1
 
-
-ciall: ci-1 ci-2 ci-3 ci-4
-
+# a quick check
 # skip lotus and any itest
 ci: ci-1 ci-3
+
+# ci more
+cimore: ci-1 ci-2 ci-3 ci-4
+
