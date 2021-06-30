cdea6769cd69bf84600b123ec4c4335485ab3c14
James Fan <james.fan@consensys.net>
2021-06-30 20:47:55 +1000

    make provider/gateway/register images in 5 seconds

--- common/scripts/60-docker-images.mk
+++ common/scripts/60-docker-images.mk
@@ -2,11 +2,11 @@
 # make micro docker images
 micro-images: register/main provider/main gateway/main
 	set -xe; \
-        for d in register provider gateway; \
-        do \
+	for d in register provider gateway; \
+	do \
 		cd $$d/micro-docker; \
-                docker rmi consensys/fc-retrieval/$$d 2>/dev/null || true; \
+		docker rmi consensys/fc-retrieval/$$d 2>/dev/null || true; \
 		cp -uv ../main .; \
 		docker build --no-cache -t consensys/fc-retrieval/$$d . ; \
 		cd ../../; \
-        done;
+	done;
